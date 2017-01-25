0fc38205148170cd2c0e600852d773b17460bbb7
Patrick Kahl <Kahl.Patrick@googlemail.com>
2017-01-25 15:18:10 +0100

    Add shell script to install routine and add install and update brew action

---
+++ sh/install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+#
+# Check if iterm2 scripts are installed
+#
+if [ ! -d "~/.iterm2" ]; then
+  curl -L https://iterm2.com/misc/install_shell_integration_and_utilities.sh | bash
+fi
+
+#
+# Install and update vim vundle plugins
+#
+vim -c VundleInstall -c quitall
+vim -c VundleUpdate -c quitall
+
+#
+# Check if Homebrew is installed
+#
+which -s brew
+if [[ $? != 0 ]] ; then
+  # Install Homebrew
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew install wget
+  brew install the_silver_searcher
+else
+  brew update
+fi
