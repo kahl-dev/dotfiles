df488c2c2aa4376a49a89862d244c7745b6b9f47
Patrick Kahl <Kahl.Patrick@googlemail.com>
2017-05-17 17:40:12 +0200

    Optimize install files

--- sh/install.sh
+++ sh/install.sh
@@ -3,38 +3,9 @@
 #
 # Check if iterm2 scripts are installed
 #
-if [ ! -d "~/.iterm2" ]; then
-  curl -L https://iterm2.com/misc/install_shell_integration_and_utilities.sh | bash
+if [ ! -d ~/.iterm2 ] || [ ! -f ~/.iterm2_shell_integration.zsh ]; then
+  curl -L https://iterm2.com/misc/install_shell_integration.sh | bash
 fi
-
-#
-# Create vim backup swap and undo folder if not exists (Workaround while git can not commit empty folder)
-#
-VIMBACKUPDIR="../vim/backup" 
-if [ ! -d "$VIMBACKUPDIR" ]; then
-  mkdir -p $VIMBACKUPDIR
-fi
-
-VIMSWAPDIR="../vim/swap" 
-if [ ! -d "$VIMSWAPDIR" ]; then
-  mkdir -p $VIMSWAPDIR
-fi
-
-VIMUNDODIR="../vim/undo" 
-if [ ! -d "$VIMUNDODIR" ]; then
-  mkdir -p $VIMUNDODIR
-fi
-
-VIMVIEWDIR="../vim/view" 
-if [ ! -d "$VIMVIEWDIR" ]; then
-  mkdir -p $VIMVIEWDIR
-fi
-
-#
-# Install and update vim vundle plugins
-#
-vim -c VundleInstall -c quitall
-vim -c VundleUpdate -c quitall
 
 if [ "$(uname)" = "Darwin" ]; then
   # Do something under Mac OS X platform
