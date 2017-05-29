e216171f0a22360a3466647a7f74e219f83b719b
Patrick Kahl <Kahl.Patrick@googlemail.com>
2017-05-29 20:20:45 +0200

    Opt. linux brew

--- sh/linux.sh
+++ sh/linux.sh
@@ -9,13 +9,27 @@ echo Handle Linux things? y/n
 read var
 if [ $var = "y" ]; then
 
-  # {{{
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+  if brew ls --versions myformula > /dev/null; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+  else
+    brew update
+    brew upgrade
+  fi
+
+  # Install packages
 
   brew install xdg-utils
   brew install curl
   brew install nodejs-legacy
+
+  # Dependency for YouCompleteMe
   brew install cmake
-  # }}}
+
+  # Ag/Ack the better grep
+  brew install the_silver_searcher
+
+  # Nice cd fuzzy search and index (Dependency for zsh plugin)
+  # https://github.com/b4b4r07/enhancd
+  brew install fzy
 
 fi
