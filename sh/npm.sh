6048cbc9fb7d391e1086ee0f57f12ef9b9ff81f9
Patrick Kahl <Kahl.Patrick@googlemail.com>
2017-06-04 10:27:00 +0200

    Add editorconfig for vim; Add osx only npm packages

--- sh/npm.sh
+++ sh/npm.sh
@@ -1,4 +1,22 @@
+#!/bin/bash
+
 # Update npm
 npm i -g npm
 
+npm i -g prettier
 npm i -g prettier-eslint-cli
+
+if [ "$(uname)" = "Darwin" ]; then
+
+  # Alfred development
+  npm i -g generator-alfred
+
+  # Alfred workflows
+  npm i -g alfred-coolors
+  npm i -g alfred-fkill
+
+  # npm i -g alfred-notifier
+  # npm i -g alfred-updater
+  # npm i -g alfy
+
+fi
