e51c8f0be3de9fc601b52907e844dd84d5938d0d
Patrick Kahl <Kahl.Patrick@googlemail.com>
2017-06-21 12:50:26 +0200

    Complete revision of tmux integration with plugin manager support

--- sh/install.sh
+++ sh/install.sh
@@ -3,6 +3,6 @@
 #
 # Check if iterm2 scripts are installed
 #
-if [ ! -d ~/.iterm2 ] || [ ! -f ~/.iterm2_shell_integration.zsh ]; then
-  curl -L https://iterm2.com/misc/install_shell_integration.sh | bash
-fi
+# if [ ! -d ~/.iterm2 ] || [ ! -f ~/.iterm2_shell_integration.zsh ]; then
+#   curl -L https://iterm2.com/misc/install_shell_integration.sh | bash
+# fi
