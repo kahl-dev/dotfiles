34a57916eeb84b1709f37551a39e1e99acf84f24
Patrick Kahl <Kahl.Patrick@googlemail.com>
2017-08-13 17:12:54 +0200

    Completly rewrite brew handling

--- sh/brew.sh
+++ sh/brew.sh
@@ -1,43 +1,78 @@
 #!/bin/sh
 
+iPkg() {
+  handler=$1
+  shift
+  array=($@)
+  for pkg in ${array[@]}; do
+    if [ "$handler" = brew ]; then
+      if ! (brew list -1 | grep -q "^${pkg}\$"); then
+          brew install $pkg
+      fi
+    elif [ "$handler" = cask ]; then
+      if ! (brew cask list -1 | grep -q "^${pkg}\$"); then
+          brew cask install $pkg
+      fi
+    fi
+  done
+}
+
+allbrew=(z git vim tmux the_silver_searcher fzy)
+osxbrew=(wget macvim reattach-to-user-namespace mongodb sshfs)
+linuxbrew=(gcc cmake)
+osxcask=(slack mongodb-compass osxfuse hyper)
+
 # Execute if not osx
-if [ ! "$(uname)" = "Darwin" ]; then
-  exit
-fi
+if [ "$(uname)" = "Darwin" ]; then
 
-echo Install Brew and Cask? y/n
-read var
-if [ $var = "y" ]; then
+  echo Do you want to install/update/upgrade brew and brew cask on os x? y/n
+  read var
+  if [ $var = "y" ]; then
 
-  # Install or update brew
-  if ! (brew --version > /dev/null); then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  else
-    brew update
-    brew upgrade
-  fi
-
-  # Install packages
-  for pkg in z git wget vim macvim tmux the_silver_searcher fzy reattach-to-user-namespace mongodb sshfs; do
-    if ! (brew list -1 | grep -q "^${pkg}\$"); then
-        brew install $pkg
+    # Install or update brew
+    if ! (brew --version > /dev/null); then
+      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    else
+      brew update
+      brew upgrade
     fi
-  done
 
-  # Cask
-  if ! (brew cask --version > /dev/null); then
-    brew tap caskroom/cask
-    brew install brew-cask
-  fi
+    # Install packages
+    pkgs=("${allbrew[@]}" "${osxbrew[@]}")
+    iPkg brew ${pkgs[@]}
 
-  # Install cask packages
-  for pkg in slack mongodb-compass osxfuse hyper; do
-    if ! (brew cask list -1 | grep -q "^${pkg}\$"); then
-        brew cask install $pkg
+    # Cask
+    if ! (brew cask --version > /dev/null); then
+      brew tap caskroom/cask
+      brew install brew-cask
     fi
-  done
 
-  brew cleanup
+    iPkg cask ${osxcask[@]}
+
+    brew cleanup
+
+  fi
 
 fi
 
+# Execute if not linux
+if [ "$(uname)" = "Linux" ]; then
+
+  echo Do you want to install/update/upgrade linuxbrew on linux? y/n
+  echo Handle Linux things? y/n
+  read var
+  if [ $var = "y" ]; then
+
+    if ! (brew --version > /dev/null); then
+      git clone https://github.com/Linuxbrew/brew.git ~/.linuxbrew
+    else
+      brew update
+      brew upgrade
+    fi
+
+    # Install packages
+    pkgs=("${allbrew[@]}" "${linuxbrew[@]}")
+    iPkg brew ${pkgs[@]}
+  fi
+
+fi
