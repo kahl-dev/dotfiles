f159f63256f65cdb0e4e284bdb1049d51f3f98d2
Patrick Kahl <Kahl.Patrick@googlemail.com>
2017-08-13 22:07:15 +0200

    Remove tmux autoload

--- zsh/settings/tmux.zsh
+++ zsh/settings/tmux.zsh
@@ -1,8 +1,8 @@
 if which tmux &> /dev/null; then
   plugins+=(tmux)
 
-  ZSH_TMUX_AUTOSTART=true
-  if [ "$(uname 2> /dev/null)" = "Darwin" ]; then
-  ZSH_TMUX_AUTOQUIT=false
-  fi
+  # ZSH_TMUX_AUTOSTART=true
+  # if [ "$(uname 2> /dev/null)" = "Darwin" ]; then
+  #   ZSH_TMUX_AUTOQUIT=false
+  # fi
 fi
