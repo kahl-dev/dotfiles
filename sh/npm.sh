b9966adce83dc0c21059887e8bffe100e5daff38
Patrick Kahl <Kahl.Patrick@googlemail.com>
2017-08-27 10:08:12 +0200

    add better nvm support

--- sh/npm.sh
+++ sh/npm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-npm i -g --quiet prettier
+# put all global packages in npm-nvm/default-packages to install in each nvm version
 
 if [ "$(uname)" = "Darwin" ]; then
 
   packages="generator-alfred alfred-coolors alfred-fkill alfred-messages alfred-notifier alfred-updater alfred-emoj"
 
   # Alfred development
-  npm install $packages --global --quiet
+  npm install $packages --global
 
 fi
