73fcc36cf45acea8ad4c0bc37390f348a9275812
Patrick Kahl <Kahl.Patrick@googlemail.com>
2017-08-28 21:47:55 +0200

    bugfix nvm

--- zsh/settings/nvm.zsh
+++ zsh/settings/nvm.zsh
@@ -1,8 +1,12 @@
-export NVM_DIR=~/.nvm
+export NVM_DIR="$HOME/.nvm"
 export NVM_LAZY_LOAD=true
 export NVM_AUTO_USE=true
-source ~/.nvm/nvm.sh
 
 plugins+=(zsh-nvm)
 
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvmInit() {
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+}
+
+after_init+=(nvmInit)
