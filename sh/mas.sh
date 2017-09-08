ff5baa7499abd480ab23793d80e34686cb0d3034
Patrick Kahl <Kahl.Patrick@googlemail.com>
2017-09-08 20:07:08 +0200

    move marked from brew to mas

--- sh/mas.sh
+++ sh/mas.sh
@@ -5,7 +5,7 @@ mas upgrade
 appstoreapps=(
 407963104 # pixelmator // photo editing app
 1063996724 # tyme // time tracking tool
-441258766 # magnet // window managment
+# 441258766 # magnet // window managment
 1107421413 # 1blocker // adblock
 1094255754 # outbank // banking app
 410968114 # pdfscanner // document scanner
@@ -13,6 +13,7 @@ appstoreapps=(
 557168941 # tweetbot // twitter app
 497799835 # xcode
 1160374471 # Pipifier // picture in picture for all videos
+890031187 # marked 2
 409203825 # numbers
 409201541 # pages
 )
