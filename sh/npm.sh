ee5cb61008b05418652894dbed4d0286be0c86d2
Patrick Kahl <Kahl.Patrick@googlemail.com>
2018-08-13 14:25:25 +0200

    Update npm install && update routine

--- sh/npm.sh
+++ sh/npm.sh
@@ -5,7 +5,7 @@
 if [ "$(uname)" = "Darwin" ]; then
 
   // alfred-vimawesome alfred-youtube 
-  packages="generator-alfred alfred-fkill alfred-messages alfred-notifier alfred-updater alfred-tyme alfred-bitly prettier eslint-config-airbnb eslint-plugin-jsx-a11y@5 eslint-plugin-react eslint-plugin-import eslint-plugin-html eslint-config-prettier eslint"
+  packages="generator-alfred alfred-fkill alfred-messages alfred-notifier alfred-updater alfred-tyme alfred-bitly alfred-polyglot prettier eslint-config-airbnb eslint-plugin-jsx-a11y@5 eslint-plugin-react eslint-plugin-import eslint-plugin-html eslint-config-prettier eslint"
 
   # Alfred development
   npm install $packages --global
