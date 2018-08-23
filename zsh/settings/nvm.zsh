8b62eae5f644ba5d3b4099c78e15868d5d6d740e
Patrick Kahl <Kahl.Patrick@googlemail.com>
2018-08-23 15:26:14 +0200

    Revise the powerlevel9k theme and set rvm and nvm to a laodalias instead of preloading

--- zsh/settings/nvm.zsh
+++ zsh/settings/nvm.zsh
@@ -1,13 +1,15 @@
 export NVM_DIR="$HOME/.nvm"
 export PATH=$NVM_DIR/versions/node/global/bin:$PATH
 export MANPATH=$NVM_DIR/versions/node/global/share/man:$MANPATH
-nvm() {
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-  nvm "${@}"
-}
+# nvm() {
+#   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#   nvm "${@}"
+# }
 
-nvmInit() {
-  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-}
+# nvmInit() {
+#   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# }
 
-after_init+=(nvmInit)
+# after_init+=(nvmInit)
+
+alias loadnvm='[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"'
--- zsh/settings/rvm.zsh
+++ zsh/settings/rvm.zsh
@@ -1,1 +1,3 @@
-PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+# PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+
+alias loadrvm='[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"'
