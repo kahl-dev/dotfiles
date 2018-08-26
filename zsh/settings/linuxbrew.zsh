d86f9909367e3496dafa4faaf10eef1c616db645
Patrick Kahl <Kahl.Patrick@googlemail.com>
2018-08-26 17:39:58 +0200

    Opt linuxbrew for zsh and vim

--- zsh/settings/linuxbrew.zsh
+++ zsh/settings/linuxbrew.zsh
@@ -1,9 +1,15 @@
 # linux only
 if [ "$(uname 2> /dev/null)" = "Linux" ] && [ -d "$HOME/.linuxbrew" ]; then
 
-  plugins+=(linuxbrew)
+  PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
+  export MANPATH="$(brew --prefix)/share/man:$MANPATH"
+  export INFOPATH="$(brew --prefix)/share/info:$INFOPATH"
+  HOMEBREW_BUILD_FROM_SOURCE=1
 
-  # use newer vim instead
-  alias vim="${HOME}/.linuxbrew/bin/vim"
+  if [ -f $HOME/bin/vim/src/vim ]; then
+    alias vim="$HOME/bin/vim/src/vim"
+  else
+    alias vim="${HOME}/.linuxbrew/bin/vim"
+  fi
 fi
 
