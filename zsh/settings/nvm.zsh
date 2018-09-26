e499f44d2f316cb11a97b280662e7c96e5bad475
Patrick Kahl <Kahl.Patrick@googlemail.com>
2018-09-26 16:18:25 +0200

    Opt. zsh nvm settings

--- zsh/settings/nvm.zsh
+++ zsh/settings/nvm.zsh
@@ -1,17 +1,21 @@
 export NVM_DIR="$HOME/.nvm"
 export PATH=$NVM_DIR/versions/node/global/bin:$PATH
 export MANPATH=$NVM_DIR/versions/node/global/share/man:$MANPATH
-nvm() {
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-  nvm "${@}"
+initNvm() {
+  if [ "$(uname 2> /dev/null)" = "Darwin" ]; then
+  . "/usr/local/opt/nvm/nvm.sh"
+  else
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    nvm "${@}"
+  fi
 }
 
-nvmInit() {
+initNvmCompletion () {
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 }
 
 # after_init+=(nvmInit)
 
-alias loadnvm='nvm > /dev/null && nvmInit > /dev/null'
+alias loadnvm='initNvm > /dev/null && initNvmCompletion > /dev/null'
 alias npmid='npm install -g yarn grunt prettier vue-cli tldr diff-so-fancy yo generator-alfred'
 alias npmida='npm install -g alfred-tyme alfred-bitly alfred-updater alfred-notifier alfred-polyglot alfred-fkill alfred-coolors alfred-npms alfred-hl'
