73f3742d3c94ed10d37ec417b0f1f69d7eb2fb3e
Patrick Kahl <Kahl.Patrick@googlemail.com>
2018-09-27 17:05:22 +0200

    Add better fzf integration

---
+++ zsh/settings/fuzzy.zsh
@@ -0,0 +1,27 @@
+# Add fzf fuzzy finder to zsh
+# https://github.com/junegunn/fzf
+
+if which fd &> /dev/null; then
+
+  # Setting fd as the default source for fzf
+  export FZF_DEFAULT_COMMAND='fd --type f'
+
+fi
+
+initFzf() {
+  [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+}
+
+after_init+=(initFzf)
+
+# Add interactive cd for fzf
+# https://github.com/changyuheng/zsh-interactive-cd
+plugins+=(zsh-interactive-cd)
+
+# Add enhanced
+# https://github.com/b4b4r07/enhancd
+initEnhanced() {
+  source $ZSH_CUSTOM/plugins/enhancd/init.sh
+}
+
+after_init+=(initEnhanced)
