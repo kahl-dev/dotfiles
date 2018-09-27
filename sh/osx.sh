739af8cc907580975f84549f035f6b2bf0a6384b
Patrick Kahl <Kahl.Patrick@googlemail.com>
2018-09-27 09:22:20 +0200

    Change mouse speed

--- sh/osx.sh
+++ sh/osx.sh
@@ -89,7 +89,7 @@ defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Set a blazingly fast keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
+defaults write NSGlobalDomain InitialKeyRepeat -int 12
 
 ### }}}
 
