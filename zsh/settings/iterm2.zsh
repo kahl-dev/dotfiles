1f59a29b965c58a549b74bea775614e5fb2d50c6
Patrick Kahl <Kahl.Patrick@googlemail.com>
2018-09-27 15:42:36 +0200

    Enable iterm over ssh

--- zsh/settings/iterm2.zsh
+++ zsh/settings/iterm2.zsh
@@ -1,1 +1,13 @@
-# test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+#! /bin/zsh
+
+if 'test -n "$SSH_CLIENT"'
+then
+
+iterm="${HOME}/.iterm2_shell_integration.zsh"
+
+if test -e $iterm \
+  curl -L https://iterm2.com/shell_integration/zsh -o $iterm
+
+source $iterm
+
+fi
