7388640a515951dd109dbb7b253cba3bd6d30d83
Patrick Kahl <Kahl.Patrick@googlemail.com>
2018-10-01 17:31:22 +0200

    Move fzf install script to zsh init

--- zsh/settings/fuzzy.zsh
+++ zsh/settings/fuzzy.zsh
@@ -6,17 +6,19 @@ if which fd &> /dev/null; then
   # Setting fd as the default source for fzf
   export FZF_DEFAULT_COMMAND='fd --type f'
 
+  initFzf() {
+    [ ! -f ~/.fzf.zsh ] && printf "%s\n" y y n | $(brew --prefix)/opt/fzf/install
+
+    source ~/.fzf.zsh
+  }
+
+  after_init+=(initFzf)
+
+  # Add interactive cd for fzf
+  # https://github.com/changyuheng/zsh-interactive-cd
+  plugins+=(zsh-interactive-cd)
 fi
 
-initFzf() {
-  [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-}
-
-after_init+=(initFzf)
-
-# Add interactive cd for fzf
-# https://github.com/changyuheng/zsh-interactive-cd
-plugins+=(zsh-interactive-cd)
 
 # Add enhanced
 # https://github.com/b4b4r07/enhancd
