848c99ebee19f8c837a74ea01e2de2ac8c39c9fe
Patrick Kahl <Kahl.Patrick@googlemail.com>
2018-10-01 18:19:13 +0200

    Update fzf installation

--- zsh/settings/fuzzy.zsh
+++ zsh/settings/fuzzy.zsh
@@ -1,15 +1,16 @@
 # Add fzf fuzzy finder to zsh
 # https://github.com/junegunn/fzf
+if which fzf &> /dev/null; then
 
-if which fd &> /dev/null; then
+  if which fd &> /dev/null; then
 
-  # Setting fd as the default source for fzf
-  export FZF_DEFAULT_COMMAND='fd --type f'
+    # Setting fd as the default source for fzf
+    export FZF_DEFAULT_COMMAND='fd --type f'
+  fi
 
   initFzf() {
-    [ ! -f ~/.fzf.zsh ] && printf "%s\n" y y n | $(brew --prefix)/opt/fzf/install
-
-    source ~/.fzf.zsh
+    [ ! -f ~/.fzf.zsh ] && $(brew --prefix)/opt/fzf/install
+    [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
   }
 
   after_init+=(initFzf)
@@ -17,13 +18,12 @@ if which fd &> /dev/null; then
   # Add interactive cd for fzf
   # https://github.com/changyuheng/zsh-interactive-cd
   plugins+=(zsh-interactive-cd)
+
+  # Add enhanced
+  # https://github.com/b4b4r07/enhancd
+  initEnhanced() {
+    # source $ZSH_CUSTOM/plugins/enhancd/init.sh
+  }
+
+  after_init+=(initEnhanced)
 fi
-
-
-# Add enhanced
-# https://github.com/b4b4r07/enhancd
-initEnhanced() {
-  source $ZSH_CUSTOM/plugins/enhancd/init.sh
-}
-
-after_init+=(initEnhanced)
