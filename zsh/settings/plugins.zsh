255e2ac4214c980bc26de4870e4754947ef963c3
Patrick Kahl <Kahl.Patrick@googlemail.com>
2018-11-15 09:35:48 +0100

    Try some zsh plugins

--- zsh/settings/plugins.zsh
+++ zsh/settings/plugins.zsh
@@ -2,4 +2,4 @@
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins+=(git gitfast osx zsh-autosuggestions alias-tips vi-mode)
+plugins+=(git gitfast git-extras osx zsh-autosuggestions alias-tips vi-mode common-aliases)
