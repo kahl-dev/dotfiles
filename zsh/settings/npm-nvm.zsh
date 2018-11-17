f184d3dc02986e95f77951e237d37a42d0c13d0a
Patrick Kahl <Kahl.Patrick@googlemail.com>
2018-11-17 22:47:29 +0100

    Add yarn auto completion and lazy loading

--- zsh/settings/npm-nvm.zsh
+++ zsh/settings/npm-nvm.zsh
@@ -17,18 +17,24 @@ nvm() {
   nvm "$@"
 }
 
+NODE_MODULES="${HOME}/.node_modules"
+PATH="$PATH:$NODE_MODULES/bin"
+
+# Unset manpath so we can inherit from /etc/manpath via the `manpath` command
+unset MANPATH
+export MANPATH="$NODE_MODULES/share/man:$(manpath)"
+
 npm() {
   unfunction "$0"
   lazynvm()
 
-  NODE_MODULES="${HOME}/.node_modules"
-  PATH="$PATH:$NODE_MODULES/bin"
-
-  # Unset manpath so we can inherit from /etc/manpath via the `manpath` command
-  unset MANPATH
-  export MANPATH="$NODE_MODULES/share/man:$(manpath)"
-
   npm "$@"
+}
+
+yarn() {
+  unfunction "$0"
+  lazynvm()
+  yarn $@
 }
 
 node() {
@@ -50,5 +56,5 @@ npx() {
 alias npmLs="npm ls --depth=0 "$@" 2>/dev/null"
 alias npmLsg="npm ls -g --depth=0 "$@" 2>/dev/null"
 
-alias npmid='npm install -g yarn grunt prettier vue-cli tldr diff-so-fancy yo generator-alfred eslint eslint-plugin-vue'
+alias npmid='npm install -g yarn yarn-completions grunt prettier vue-cli tldr diff-so-fancy yo generator-alfred eslint eslint-plugin-vue'
 alias npmida='npm install -g alfred-tyme alfred-bitly alfred-updater alfred-notifier alfred-polyglot alfred-fkill alfred-coolors alfred-npms alfred-hl'
