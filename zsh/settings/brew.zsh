519157097c5c89c4052d89ccc2339484fb8d854f
Patrick Kahl <Kahl.Patrick@googlemail.com>
2018-12-05 10:13:22 +0100

    Bugfix linuxbrew

--- zsh/settings/brew.zsh
+++ zsh/settings/brew.zsh
@@ -1,7 +1,7 @@
 # linux only
 if [ "$(uname 2> /dev/null)" = "Linux" ] && [ -d "$HOME/.linuxbrew" ]; then
 
-  PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
+  export PATH="$HOME/.linuxbrew/bin:$PATH"
   export MANPATH="$(brew --prefix)/share/man:$MANPATH"
   export INFOPATH="$(brew --prefix)/share/info:$INFOPATH"
   HOMEBREW_BUILD_FROM_SOURCE=1
@@ -9,10 +9,11 @@ if [ "$(uname 2> /dev/null)" = "Linux" ] && [ -d "$HOME/.linuxbrew" ]; then
   #alias vim="${HOME}/.linuxbrew/bin/vim"
 fi
 
-export PATH="/usr/local/sbin:$PATH"
 
 # https://docs.brew.sh/Shell-Completion
 # Brew auto completion
-if type brew &>/dev/null; then
-  FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
+if [ "$(uname 2> /dev/null)" = "Darwin" ]; then
+  if type brew &>/dev/null; then
+    FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
+  fi
 fi
