033029beb5c0887dfd0418c1a50b1504b699e5d0
Patrick Kahl <Kahl.Patrick@googlemail.com>
2019-04-16 10:18:55 +0200

    Update eslint

--- ee/eslintrc.js
+++ ee/eslintrc.js
@@ -7,6 +7,10 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint',
     sourceType: 'module',
+    ecmaVersion: 2017,
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+    },
   },
   extends: ['plugin:prettier/recommended'],
   // required to lint *.vue files
