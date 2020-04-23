8150dfbe33dd29b292165fe63a80cf7e5924690b
Patrick Kahl <Kahl.Patrick@googlemail.com>
2020-04-23 11:42:59 +0200

    Add zsh-syntax-highlighting

--- modules/zsh-settings/plugins.zsh
+++ modules/zsh-settings/plugins.zsh
@@ -2,4 +2,4 @@
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins+=(git gitfast osx zsh-autosuggestions alias-tips zsh-better-npm-completion yarn)
+plugins+=(git gitfast osx zsh-autosuggestions alias-tips zsh-better-npm-completion yarn zsh-syntax-highlighting)
