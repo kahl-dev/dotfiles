6ead9f3bde7fca4a03a578c0ed4681b6b5baf2ed
Patrick Kahl <Kahl.Patrick@googlemail.com>
2020-04-23 16:38:07 +0200

    Disable auto git update dotfiles

--- modules/zsh-settings/git.zsh
+++ modules/zsh-settings/git.zsh
@@ -1,6 +1,6 @@
 # Git pull dotfiles repo recursive
-(git -C $HOME/.dotfiles pull &) &> /dev/null
-(git -C $HOME/.dotfiles submodule update --remote --merge --init &) &> /dev/null
+# (git -C $HOME/.dotfiles pull &) &> /dev/null
+# (git -C $HOME/.dotfiles submodule update --remote --merge --init &) &> /dev/null
 
 gde() {
   git diff -- . ':(exclude)*.min.js' ':(exclude)*.min.css'
