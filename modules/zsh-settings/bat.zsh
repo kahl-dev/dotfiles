6bf6e19eb29843aa9280600465a00ba2780856b6
Patrick Kahl <Kahl.Patrick@googlemail.com>
2020-07-19 11:55:51 +0200

    Bugfix bat

--- modules/zsh-settings/bat.zsh
+++ modules/zsh-settings/bat.zsh
@@ -1,6 +1,12 @@
 # Use bat instead of cat
-if which bat >/dev/null 2>&1; then
-  export BAT_CONFIG_PATH="$DOTFILES/ee/bat.conf"
-  export MANPAGER="sh -c 'col -bx | bat -l man -p'"
-  alias cat='bat'; 
-fi
+initBat() {
+
+  if binaryExists bat; then
+    export BAT_CONFIG_PATH="$DOTFILES/ee/bat.conf"
+    export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+    alias cat='bat'; 
+  fi
+
+}
+
+after_init+=(initBat)
