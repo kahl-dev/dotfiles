0e9f6e69f8bb468405c9dbed8aaa52ec5f9145c5
Patrick Kahl <Kahl.Patrick@googlemail.com>
2020-09-30 09:14:38 +0200

    Add nvm current symlink

--- modules/nvm.zsh
+++ modules/nvm.zsh
@@ -4,6 +4,7 @@
 export NVM_LAZY_LOAD=false
 # export NVM_LAZY_LOAD_EXTRA_COMMANDS=('vim')
 export NVM_AUTO_USE=true
+export NVM_SYMLINK_CURRENT=true
 
 zinit ice wait"2" lucid
 zinit load lukechilds/zsh-nvm
