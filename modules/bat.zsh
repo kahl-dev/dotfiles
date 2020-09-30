cd48557ecdaaa8211b4925412dfc733a5f2dd2b0
Patrick Kahl <Kahl.Patrick@googlemail.com>
2020-09-30 14:25:02 +0200

    Remove bat alias to bugfix nvm auto change

--- modules/bat.zsh
+++ modules/bat.zsh
@@ -8,7 +8,7 @@ function initBat() {
   export BAT_CONFIG_PATH="$DOTFILES/config/bat.conf"
   export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 
-  alias cat='bat';
+  # alias cat='bat';
 }
 
 zinit ice as"command" from"gh-r" mv"bat* -> bat" pick"bat/bat" atload"initBat"
