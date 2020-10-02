5001eca2622519f3ea55ac42e36a08f239e80a18
Patrick Kahl <Kahl.Patrick@googlemail.com>
2020-10-02 21:51:08 +0200

    Add nvm completion

--- modules/nvm.zsh
+++ modules/nvm.zsh
@@ -5,6 +5,7 @@ export NVM_LAZY_LOAD=false
 # export NVM_LAZY_LOAD_EXTRA_COMMANDS=('vim')
 export NVM_AUTO_USE=true
 export NVM_SYMLINK_CURRENT=true
+export NVM_COMPLETION=true
 
 function initNvm {
   [ ! -d "$HOME/.nvm/default-packages" ] && cp $DOTFILES/config/default-packages $HOME/.nvm/default-packages
