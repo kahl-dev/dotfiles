8ce57bb07e018c06b7af52c8b61582b14234799a
Patrick Kahl <Kahl.Patrick@googlemail.com>
2020-10-26 11:03:34 +0100

    Rewrite zini vim make to run on remote ubuntu

--- modules/vim.zsh
+++ modules/vim.zsh
@@ -7,6 +7,7 @@ function initVim {
     export VIMRUNTIME="$ZINIT[PLUGINS_DIR]/vim---vim/runtime"
 }
 
-zinit ice as"program" atload"initVim" atclone"rm -f src/auto/config.cache; ./configure" \
-    atpull"%atclone" make pick"src/vim"
+zinit ice as"program" atclone"rm -f src/auto/config.cache; \
+    ./configure --prefix=$ZPFX" atpull"%atclone" \
+    make"all install" pick"$ZPFX/bin/vim"
 zinit light vim/vim
