97fc785b8ad725f97057b6165f1bb19f2459887e
Patrick Kahl <Kahl.Patrick@googlemail.com>
2020-10-30 20:41:41 +0100

    Only remove background on remote machines

--- modules/themes.zsh
+++ modules/themes.zsh
@@ -5,7 +5,10 @@ zinit light romkatv/powerlevel10k
 
 # An architecture for building themes based on carefully chosen syntax highlighting using a base of sixteen colors.
 # Doc: https://github.com/chriskempson/base16
-export BASE16_SHELL_SET_BACKGROUND=false
+if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+  export BASE16_SHELL_SET_BACKGROUND=false
+fi
+
 zinit ice wait"2" lucid
 zinit load chriskempson/base16-shell
 
