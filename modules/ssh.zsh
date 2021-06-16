d34c8e2bfab74463219cdc478f84eba23b6849fc
Patrick Kahl <Kahl.Patrick@googlemail.com>
2021-06-16 20:46:08 +0200

    Bugfix tmux ssh-key integration

--- modules/ssh.zsh
+++ modules/ssh.zsh
@@ -1,3 +1,4 @@
-if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
-  export SSH_AUTH_SOCK=$HOME/.ssh/ssh_auth_sock
-fi
+# Look into tmux remote
+# if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+#   export SSH_AUTH_SOCK=$HOME/.ssh/ssh_auth_sock
+# fi
