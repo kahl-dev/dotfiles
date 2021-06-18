ddaa4906f1a01eab2c99c9eab094b538f04061d5
Patrick Kahl <Kahl.Patrick@googlemail.com>
2021-06-18 10:24:50 +0200

    Remove alfred workflows

--- modules/npm.zsh
+++ modules/npm.zsh
@@ -3,4 +3,3 @@ alias npmLs="npm ls --depth=0 "$@" 2>/dev/null"
 alias npmLsg="npm ls -g --depth=0 "$@" 2>/dev/null"
 
 alias npmid='npm install -g yarn yarn-completions npm-check grunt prettier vue-cli tldr diff-so-fancy yo generator-alfred babel-eslint eslint eslint-config-prettier eslint-plugin-html eslint-plugin-prettier eslint-plugin-vue serve inspect-process serverless browser-sync'
-alias npmida='npm install -g alfred-updater alfred-notifier alfred-polyglot'
