c352ab210de81ce6623414c8e92beafbb509e1c0
Patrick Kahl <Kahl.Patrick@googlemail.com>
2021-07-23 10:32:11 +0200

    Clean up fzf file

--- modules/fzf.zsh
+++ modules/fzf.zsh
@@ -11,55 +11,7 @@ initFzf() {
   # export FZF_CTRL_T_OPTS="--preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2> /dev/null | head -200'"
   export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
-
-  # tm - create new tmux session, or switch to existing one. Works from within tmux too. (@bag-man)
-  # `tm` will allow you to select your tmux session via fzf.
-  # `tm irc` will attach to the irc session (if it exists), else it will create it.
-  fzf_tmux() {
-    [[ -n "$TMUX" ]] && change="switch-client" || change="attach-session"
-    if [ $1 ]; then
-      tmux $change -t "$1" 2>/dev/null || (tmux new-session -d -s $1 && tmux $change -t "$1"); return
-    fi
-    session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0) &&  tmux $change -t "$session" || echo "No sessions found."
-  }
-
-  # ftpane - switch pane (@george-b)
-  fzf_tmux_pane() {
-    local panes current_window current_pane target target_window target_pane
-    panes=$(tmux list-panes -s -F '#I:#P - #{pane_current_path} #{pane_current_command}')
-    current_pane=$(tmux display-message -p '#I:#P')
-    current_window=$(tmux display-message -p '#I')
-
-    target=$(echo "$panes" | grep -v "$current_pane" | fzf +m --reverse) || return
-
-    target_window=$(echo $target | awk 'BEGIN{FS=":|-"} {print$1}')
-    target_pane=$(echo $target | awk 'BEGIN{FS=":|-"} {print$2}' | cut -c 1)
-
-    if [[ $current_window -eq $target_window ]]; then
-      tmux select-pane -t ${target_window}.${target_pane}
-    else
-      tmux select-pane -t ${target_window}.${target_pane} &&
-        tmux select-window -t $target_window
-    fi
-  }
-
-  # fbr - checkout git branch (including remote branches), sorted by most recent commit, limit 30 last branches
-  fzf_git_branch() {
-    local branches branch
-    branches=$(git branch -a --sort=-committerdate --format="%(refname:short)") &&
-    branch=$(echo "$branches" |
-    fzf -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
-    git checkout --track $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
-  }
-
   alias falias='alias | fzf'
-
-  # fzf tmux
-  alias ft="fzf_tmux"
-  alias ftp="fzf_tmux_pane"
-
-  # fzf git
-  alias fgco='fzf_git_branch'
 }
 
 # zinit atload"initFzf" pack"default" for fzf
