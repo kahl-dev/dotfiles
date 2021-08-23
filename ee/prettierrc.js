0ebc995317567174fffc26902c5822a7593baca9
Patrick Kahl <Kahl.Patrick@googlemail.com>
2021-08-23 12:22:46 +0200

    Update prettierrc

--- ee/prettierrc.js
+++ ee/prettierrc.js
@@ -1,7 +1,21 @@
 // prettier.config.js or .prettierrc.js
 module.exports = {
-  printWidth: 80,
   semi: false,
   singleQuote: true,
   trailingComma: 'es5',
+  jsxBracketSameLine: true,
+  overrides: [
+    {
+      files: ['*.js'],
+      options: {
+        printWidth: 80,
+      },
+    },
+    {
+      files: ['*.html'],
+      options: {
+        printWidth: 140,
+      },
+    },
+  ],
 }
