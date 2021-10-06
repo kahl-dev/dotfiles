82022e1ada128a6b534ab375a15c4071f1c6104f
Patrick Kahl <Kahl.Patrick@googlemail.com>
2021-10-06 20:10:00 +0200

    Bugfix tmux autoinstall; Add post zsh file support; Move auto tmux

---
+++ modules/post-tmux.zsh
@@ -0,0 +1,10 @@
+if [ -z "$TMUX" ]
+then
+
+   if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+      cd ~ && tmux attach -t REMOTE || tmux new -s REMOTE
+   # else
+   #    cd ~ && tmux attach -t LOCAL || tmux new -s LOCAL
+   fi
+
+fi
