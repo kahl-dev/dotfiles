af2486ee91658c806ef0740c68f77aa442966b9f
Patrick Kahl <Kahl.Patrick@googlemail.com>
2021-10-06 18:24:16 +0200

    Remove auto local tmux

--- modules/themes.zsh
+++ modules/themes.zsh
@@ -19,8 +19,8 @@ function base16ShellUpdate {
   then
     if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
       cd ~ && tmux attach -t REMOTE || tmux new -s REMOTE
-    else
-      # cd ~ && tmux attach -t LOCAL || tmux new -s LOCAL
+    # else
+    #   cd ~ && tmux attach -t LOCAL || tmux new -s LOCAL
     fi
   fi
 }
