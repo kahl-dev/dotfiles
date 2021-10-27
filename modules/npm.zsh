8ef89a2853aff7f1d14e52b91a042f47d3c82e14
Patrick Kahl <Kahl.Patrick@googlemail.com>
2021-10-27 17:52:31 +0200

    Rewrite npmid to work on all platforms

--- modules/npm.zsh
+++ modules/npm.zsh
@@ -3,5 +3,5 @@ alias npmLs="npm ls --depth=0 "$@" 2>/dev/null"
 alias npmLsg="npm ls -g --depth=0 "$@" 2>/dev/null"
 
 npmid() {
- xargs -a $DOTFILES/config/default-packages npm install -g
+  cat $DOTFILES/config/default-packages | xargs npm install -g
 }
