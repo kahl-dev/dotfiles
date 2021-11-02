895c53beb35dbcd2f329dfe66a2eccceb226cd3b
Patrick Kahl <Kahl.Patrick@googlemail.com>
2021-11-02 11:05:43 +0100

    Add git helper

---
+++ modules/git.zsh
@@ -0,0 +1,6 @@
+zinit as"null" wait"1" lucid for \
+    sbin    Fakerr/git-recall \
+    sbin    cloneopts paulirish/git-open \
+    sbin    paulirish/git-recent \
+    sbin    davidosomething/git-my \
+    sbin    iwata/git-now \
