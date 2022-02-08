c2f51b3404e4a4c3a22e95b56062d6773eb58b81
Patrick Kahl <Kahl.Patrick@googlemail.com>
2022-02-08 17:31:55 +0100

    Rewrite fzf and ripgrep integration

--- modules/ripgrep.zsh
+++ modules/ripgrep.zsh
@@ -1,13 +1,7 @@
 # ripgrep is a line-oriented search tool that recursively searches your current directory for a regex pattern.
 # Doc: https://github.com/BurntSushi/ripgrep/
 
-function initRipgrep() {
+if _exists rg; then
   export RIPGREP_CONFIG_PATH=$DOTFILES/config/ripgreprc
-}
-
-if _not_exists rg; then
-  zinit ice atload="initRipgrep" as"command" from"gh-r" mv"ripgrep* -> rg" pick"rg/rg"
-  zinit light BurntSushi/ripgrep
-else
-  zinit wait'1' lucid atinit'initRipgrep' nocd for /dev/null
+  plugins+=(ripgrep)
 fi
