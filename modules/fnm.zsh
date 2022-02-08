01651e62017675d84cf24b378b2e80048b7e9438
Patrick Kahl <Kahl.Patrick@googlemail.com>
2022-02-08 19:13:04 +0100

    Refactor fnm integration

--- modules/fnm.zsh
+++ modules/fnm.zsh
@@ -1,19 +1,12 @@
-function initFnm() {
-  [ ! -d "$HOME/.fnm" ] && curl -fsSL https://fnm.vercel.app/install | bash -s -- --install-dir "$HOME/.fnm" --skip-shell
+if _not_exists fnm; then
+  curl -fsSL https://fnm.vercel.app/install | bash -s -- --install-dir "$DOTFILES/bin/fnm" --skip-shell
+fi
 
-
-  export PATH=$HOME/.fnm:$PATH
-  eval "`fnm env`"
-
+if _exists fnm; then
   export NODE_PATH=$FNM_DIR/aliases/latest
   [[ -f "$NODE_PATH/lib/node_modules/yarn-completions/node_modules/tabtab/.completions/yarn.zsh" ]] && . "$NODE_PATH/lib/node_modules/yarn-completions/node_modules/tabtab/.completions/yarn.zsh"
 
-  if [ ! -f "$HOME/.config/zsh/completions/_fnm" ]; then
-    mkdir -p $HOME/.config/zsh/completions
-    touch $HOME/.config/zsh/completions/_fnm
-    fnm completions --shell=zsh > $HOME/.config/zsh/completions/_fnm
-  fi
-  zinit fpath -f $HOME/.config/zsh/completions/_fnm
+  plugins+=(fnm)
 
   find-up () {
     path=$(pwd)
@@ -40,6 +33,4 @@ function initFnm() {
 
   add-zsh-hook chpwd load-nvmrc
   load-nvmrc
-}
-
-zinit lucid atinit'initFnm' nocd for /dev/null
+fi
