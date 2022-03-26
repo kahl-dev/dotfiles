89e6c0f80409acb8e7cddd669dbd8c9a69bf7217
Patrick Kahl <Kahl.Patrick@googlemail.com>
2022-03-26 17:57:46 +0100

    Disable 1password ssh agent

--- modules/ssh.zsh
+++ modules/ssh.zsh
@@ -5,14 +5,14 @@
 #
 
 # Add 1password support for ssh
-if [ "$(uname)" = "Darwin" ]; then
-  if [ -d "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t" ]; then
-    if [ ! -L "$HOME/.1password/agent.sock" ]; then
-      mkdir -p ~/.1password && ln -s ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock ~/.1password/agent.sock
-    fi
+# if [ "$(uname)" = "Darwin" ]; then
+#   if [ -d "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t" ]; then
+#     if [ ! -L "$HOME/.1password/agent.sock" ]; then
+#       mkdir -p ~/.1password && ln -s ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock ~/.1password/agent.sock
+#     fi
 
-    if [ -L "$HOME/.1password/agent.sock" ]; then
-      export SSH_AUTH_SOCK=~/.1password/agent.sock
-    fi
-  fi
-fi
+#     if [ -L "$HOME/.1password/agent.sock" ]; then
+#       export SSH_AUTH_SOCK=~/.1password/agent.sock
+#     fi
+#   fi
+# fi
