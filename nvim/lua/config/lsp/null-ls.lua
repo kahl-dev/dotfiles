880ac216921f1d36df057e2b5bc075642e5388c6
Patrick Kahl <Kahl.Patrick@googlemail.com>
2022-09-07 11:32:55 +0200

    Add auto format on save for null-ls

--- nvim/lua/config/lsp/null-ls.lua
+++ nvim/lua/config/lsp/null-ls.lua
@@ -9,22 +9,24 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-	debug = false,
+	debug = true,
 	sources = {
 		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+
 		formatting.stylua,
-		-- diagnostics.flake8
 	},
 
-	-- you can reuse a shared lspconfig on_attach callback here
-	-- on_attach = function(client)
-	-- 	if client.resolved_capabilities.document_formatting then
-	-- 		vim.cmd([[
-	--        augroup LspFormatting
-	--            autocmd! * <buffer>
-	--            autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
-	--        augroup END
-	--        ]])
-	-- 	end
-	-- end,
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+					vim.lsp.buf.format({ async = false })
+				end,
+			})
+		end
+	end,
 })
