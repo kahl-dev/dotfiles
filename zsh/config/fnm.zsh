ad00960605421f426fc759705038c38031268d30
Patrick Kahl <Kahl.Patrick@googlemail.com>
2022-09-27 23:44:55 +0200

    Update brew files and fnm integration

--- zsh/config/fnm.zsh
+++ zsh/config/fnm.zsh
@@ -1,22 +1,20 @@
-if [ -d "$DOTFILES/bin/fnm" ]; then
-  export PATH=$DOTFILES/bin/fnm:$PATH
-  eval "`fnm env`"
-fi
+# if [ -d "$DOTFILES/bin/fnm" ]; then
+#   export PATH=$DOTFILES/bin/fnm:$PATH
+#   eval "`fnm env`"
+# fi
 
-if ! _exec_exists fnm; then
-  curl -fsSL https://fnm.vercel.app/install | bash -s -- --install-dir "$DOTFILES/bin/fnm" --skip-shell
-fi
+# if ! _exec_exists fnm; then
+#   curl -fsSL https://fnm.vercel.app/install | bash -s -- --install-dir "$DOTFILES/bin/fnm" --skip-shell
+# fi
 
 if _exec_exists fnm; then
-  export FNM_DIR="$DOTFILES/bin/fnm"
+  eval "$(fnm env --use-on-cd --version-file-strategy=recursive)"
 
-  if [ ! -d "$FNM_DIR/aliases/lts-latest" ]; then
+  if [ ! -L "$FNM_DIR/aliases/lts-latest" ]; then
     fnm install --lts
+    fnm use lts-latest
     cat $DOTFILES/config/default-packages | xargs npm install -g
   fi
   export NODE_PATH=$FNM_DIR/aliases/lts-latest
   [[ -f "$NODE_PATH/lib/node_modules/yarn-completions/node_modules/tabtab/.completions/yarn.zsh" ]] && . "$NODE_PATH/lib/node_modules/yarn-completions/node_modules/tabtab/.completions/yarn.zsh"
-
-  plugins+=(fnm)
-  eval "$(fnm env --use-on-cd --version-file-strategy=recursive)"
 fi
