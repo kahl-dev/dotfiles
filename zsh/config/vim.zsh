0f257182b784366a74d5d94beb4e9c979fe2e4ec
Patrick Kahl <Kahl.Patrick@googlemail.com>
2022-10-10 11:11:24 +0200

    Update vim alias

--- zsh/config/vim.zsh
+++ zsh/config/vim.zsh
@@ -5,8 +5,13 @@ if _exec_exists nvim; then
   # alias vim='nvm use default && nvim'
   # alias vim='fnm exec --using=v16 nvim'
 
+
   function v() {
-    command fnm exec --using=v16 nvim "$@"
+    if _exec_exists fnm; then
+      command fnm exec --using=v16 nvim "$@"
+    else
+      nvim "$@"
+    fi
   }
 
   alias vim='v'
