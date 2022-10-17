a18a76f108adf494b877c2169d416d75efcf9d79
Patrick Kahl <Kahl.Patrick@googlemail.com>
2022-10-17 19:43:53 +0200

    Update nvim-tree config

--- nvim/lua/config/nvim-tree.lua
+++ nvim/lua/config/nvim-tree.lua
@@ -5,4 +5,16 @@ if not status_ok then
 	return
 end
 
-nvim_tree.setup({})
+vim.g.loaded = 1
+vim.g.loaded_netrwPlugin = 1
+
+nvim_tree.setup({
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
+	},
+	diagnostics = {
+		enable = true,
+		show_on_dirs = true,
+	},
+})
