d7cd977e4ae3baeab468037fcbf76867054e76ae
Patrick Kahl <Kahl.Patrick@googlemail.com>
2023-02-08 17:54:18 +0100

    Bugfix colorizer integration

--- nvim/lua/config/colorizer.lua
+++ nvim/lua/config/colorizer.lua
@@ -7,4 +7,17 @@ end
 
 colorizer.setup({
 	"scss",
+	"css",
+	"html",
+	"vue",
+}, {
+	RGB = true,
+	RRGGBB = true,
+	names = true,
+	RRGGBBAA = true,
+	rgb_fn = true,
+	hsl_fn = true,
+	css = true,
+	css_fn = true,
+	mode = "background",
 })
