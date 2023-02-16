cefc21036e0ba6b5002e377e9180fefb269a468a
Patrick Kahl <Kahl.Patrick@googlemail.com>
2023-02-16 21:58:12 +0100

    Try out lazyvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,2 @@
--- Plugin management via Packer
-require("plugins").setup()
--- Vim mappings, see lua/config/which.lua for more mappings
-require("mappings")
--- All non plugin related (vim) options
-require("options")
--- Vim autocommands/autogroups
-require("autocmd")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
