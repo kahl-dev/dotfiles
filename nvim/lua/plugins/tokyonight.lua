ecc58a7cc1d61e42987a12b416c44b4c1cf9fb61
Patrick Kahl <Kahl.Patrick@googlemail.com>
2023-02-19 11:32:53 +0100

    Add transparency with tokyonight

---
+++ nvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,15 @@
+-- A clean, dark Neovim theme written in Lua, with support for lsp, treesitter
+-- and lots of plugins. Includes additional themes for Kitty, Alacritty,
+-- iTerm and Fish.
+-- https://github.com/folke/tokyonight.nvim
+
+return {
+  "folke/tokyonight.nvim",
+  opts = {
+    transparent = true,
+    styles = {
+      sidebase = "transparent",
+      floats = "transparent",
+    },
+  },
+}
