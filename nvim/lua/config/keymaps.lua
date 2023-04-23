3a1a7131ac499d841b447bcd36727676872c7eb9
Patrick Kahl <Kahl.Patrick@googlemail.com>
2023-04-23 14:36:47 +0200

    Add shell gpt and opt neovim integration

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,9 +2,31 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local Util = require("lazyvim.util")
+
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+if Util.has("bufferline.nvim") then
+  map("n", "<leader>cgc", "<cmd>ChatGPT<CR>", { desc = "ChatGPT" })
+  map("n", "<leader>cga", "<cmd>ChatGPTAct<CR>", { desc = "ChatGPTAct" })
+  map("n", "<leader>cgi", "<cmd>ChatGPTEditWithInstructions<CR>", { desc = "ChatGPTEditWithInstructions" })
+  map("v", "<leader>cgi", "<cmd>ChatGPTEditWithInstructions<CR>", { desc = "ChatGPTEditWithInstructions" })
+  -- map("n", "<leader>cgr", "<cmd>ChatGPTRun<CR>", { desc = "ChatGPTRun" })
+  -- map("v", "<leader>cgr", "<cmd>ChatGPTRun<CR>", { desc = "ChatGPTRun" })
+end
+
 vim.cmd([[
   inoremap jk <ESC>
- 
+
   noremap <silent> <c-h> :<C-U>TmuxNavigateLeft<cr>
   noremap <silent> <c-j> :<C-U>TmuxNavigateDown<cr>
   noremap <silent> <c-k> :<C-U>TmuxNavigateUp<cr>
---
+++ nvim/lua/plugins/chat-gpt.lua
@@ -0,0 +1,28 @@
+-- Description: Chat with GPT-3
+-- https://github.com/jackMort/ChatGPT.nvim
+return {
+  "jackMort/ChatGPT.nvim",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  config = function()
+    require("chatgpt").setup({
+      chat = {
+        keymaps = {
+          close = { "jk", "kj", "<Esc>" },
+          yank_last = "<C-y>",
+          scroll_up = "<C-u>",
+          scroll_down = "<C-d>",
+          toggle_settings = "<C-o>",
+          new_session = "<C-n>",
+          cycle_windows = "<Tab>",
+        },
+      },
+      popup_input = {
+        submit = "<C-s>",
+      },
+    })
+  end,
+}
