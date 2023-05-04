e86f5c731b2ec1ec9211fd1c93560b8d2057a033
Patrick Kahl <Kahl.Patrick@googlemail.com>
2023-05-04 09:26:50 +0200

    Add emmet language server to neovim

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -11,6 +11,21 @@ return {
         docker_compose_language_service = {},
         prismals = {},
         shellcheck = {},
+        -- https://github.com/aca/emmet-ls
+        emmet_ls = {
+          -- filetype = { "html", "css", "scss", "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
+          -- init_options = {
+          --   config = {
+          --     emmet = {
+          --       variables = {
+          --         ["lang.scss"] = "scss",
+          --         ["lang.less"] = "less",
+          --         ["lang.stylus"] = "stylus",
+          --       },
+          --     },
+          --   },
+          -- },
+        },
       },
       format = {
         async = true,
