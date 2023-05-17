63b0ed5247d8b9f418a2cf468313b756723be0cf
Patrick Kahl <Kahl.Patrick@googlemail.com>
2023-05-17 16:21:17 +0200

    Hopefully add multiline support for yanking over ssh

--- scripts/nc_listener.sh
+++ scripts/nc_listener.sh
@@ -5,13 +5,15 @@ log_file="$HOME/Library/Logs/com.kahl_dev.nc_listener"
 
 # A simple regex to check for a URL-like string.
 # This is not fully accurate, but should work for most common URLs.
-url_regex='^https?://'
+url_regex='^https?://[-A-Za-z0-9+&@#/%?=~_|!:,.;]*[-A-Za-z0-9+&@#/%=~_|]$'
 
-while read line; do
-	echo "$line" | pbcopy
-	echo "$(date) - Received: $line - Copied to clipboard" >>$log_file
-	if [[ $line =~ $url_regex ]]; then
-		open "$line"
-		echo "$(date) - Opened in browser: $line" >>$log_file
+while IFS= read -r line; do
+	# Decode the Base64 input
+	block=$(echo "$line" | base64 --decode)
+	echo "$block" | pbcopy
+	echo "$(date) - Received: $block - Copied to clipboard" >>$log_file
+	if [[ $block =~ $url_regex ]]; then
+		open "$block"
+		echo "$(date) - Opened in browser: $block" >>$log_file
 	fi
 done
