969cc663c9f95e24ff88d682bad8484d33f495d3
Patrick Kahl <Kahl.Patrick@googlemail.com>
2023-05-19 22:46:52 +0200

    Update update script

--- scripts/updates.sh
+++ scripts/updates.sh
@@ -1,5 +1,11 @@
-# https://www.chriswrites.com/update-apps-and-macos-without-ever-launching-the-app-store/
 #!/bin/bash
+
+source ./scripts/config.sh
+source ./scripts/functions.sh
+
+# https://www.chriswrites.com/update-apps-and-macos-without-ever-launching-the-app-store/
+
+printf "${COLOR_CYAN}Update Homebrew packages${COLOR_OFF}\n"
 brew update
 brew upgrade
 brew cleanup -s
@@ -7,6 +13,8 @@ brew cleanup -s
 brew doctor
 brew missing
 
+printf "${COLOR_CYAN}Update App Store packages with mas${COLOR_OFF}\n"
 mas upgrade
 
+printf "${COLOR_CYAN}Update System${COLOR_OFF}\n"
 softwareupdate -i -a
