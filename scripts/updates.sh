7b9da646d80f0c626167f3959cfad30d7536c27e
Patrick Kahl <Kahl.Patrick@googlemail.com>
2023-05-21 17:40:27 +0200

    Update update script to choose what to update

--- scripts/updates.sh
+++ scripts/updates.sh
@@ -3,21 +3,31 @@
 source ./scripts/config.sh
 source ./scripts/functions.sh
 
-printf "${COLOR_CYAN}Update LazyVim${COLOR_OFF}\n"
-nvim --headless '+Lazy! sync' +qa
+read -p "Do you want to update LazyVim? (y/n) " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+	printf "${COLOR_CYAN}Update LazyVim${COLOR_OFF}\n"
+	nvim --headless '+Lazy! sync' +qa
+fi
 
-# https://www.chriswrites.com/update-apps-and-macos-without-ever-launching-the-app-store/
+read -p "Do you want to update Homebrew packages? (y/n) " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+	printf "${COLOR_CYAN}Update Homebrew packages${COLOR_OFF}\n"
+	brew update
+	brew upgrade
+	brew cleanup -s
+	#now diagnotic
+	brew doctor
+	brew missing
+fi
 
-printf "${COLOR_CYAN}Update Homebrew packages${COLOR_OFF}\n"
-brew update
-brew upgrade
-brew cleanup -s
-#now diagnotic
-brew doctor
-brew missing
+read -p "Do you want to update App Store packages with mas? (y/n) " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+	printf "${COLOR_CYAN}Update App Store packages with mas${COLOR_OFF}\n"
+	mas upgrade
+fi
 
-printf "${COLOR_CYAN}Update App Store packages with mas${COLOR_OFF}\n"
-mas upgrade
-
-printf "${COLOR_CYAN}Update System${COLOR_OFF}\n"
-softwareupdate -i -a
+read -p "Do you want to update the System? (y/n) " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+	printf "${COLOR_CYAN}Update System${COLOR_OFF}\n"
+	softwareupdate -i -a
+fi
