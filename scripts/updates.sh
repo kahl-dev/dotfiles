f5b996f64c3af86efb3c3bebee8ad8cda37daad3
Patrick Kahl <Kahl.Patrick@googlemail.com>
2023-05-21 17:48:11 +0200

    Update update script

--- scripts/updates.sh
+++ scripts/updates.sh
@@ -9,27 +9,33 @@ if [[ $1 == '--yes' ]] || [[ $1 == '-y' ]]; then
 	all_yes=1
 fi
 
-if [[ $all_yes == 1 ]] || (read -p "Do you want to update LazyVim? (y/n) " response && [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]); then
-	printf "${COLOR_CYAN}Update LazyVim${COLOR_OFF}\n"
-	nvim --headless '+Lazy! sync' +qa
+if _exec_exists nvim; then
+	if [[ $all_yes == 1 ]] || (read -p "Do you want to update LazyVim? (y/n) " response && [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]); then
+		printf "${COLOR_CYAN}Update LazyVim${COLOR_OFF}\n"
+		nvim --headless '+Lazy! sync' +qa
+	fi
 fi
 
-if [[ $all_yes == 1 ]] || (read -p "Do you want to update Homebrew packages? (y/n) " response && [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]); then
-	printf "${COLOR_CYAN}Update Homebrew packages${COLOR_OFF}\n"
-	brew update
-	brew upgrade
-	brew cleanup -s
-	#now diagnotic
-	brew doctor
-	brew missing
+if _exec_exists brew; then
+	if [[ $all_yes == 1 ]] || (read -p "Do you want to update Homebrew packages? (y/n) " response && [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]); then
+		printf "${COLOR_CYAN}Update Homebrew packages${COLOR_OFF}\n"
+		brew update
+		brew upgrade
+		brew cleanup -s
+		#now diagnotic
+		brew doctor
+		brew missing
+	fi
 fi
 
-if [[ $all_yes == 1 ]] || (read -p "Do you want to update App Store packages with mas? (y/n) " response && [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]); then
-	printf "${COLOR_CYAN}Update App Store packages with mas${COLOR_OFF}\n"
-	mas upgrade
-fi
+if _is_osx; then
+	if [[ $all_yes == 1 ]] || (read -p "Do you want to update App Store packages with mas? (y/n) " response && [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]); then
+		printf "${COLOR_CYAN}Update App Store packages with mas${COLOR_OFF}\n"
+		mas upgrade
+	fi
 
-if [[ $all_yes == 1 ]] || (read -p "Do you want to update the System? (y/n) " response && [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]); then
-	printf "${COLOR_CYAN}Update System${COLOR_OFF}\n"
-	softwareupdate -i -a
+	if [[ $all_yes == 1 ]] || (read -p "Do you want to update the System? (y/n) " response && [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]); then
+		printf "${COLOR_CYAN}Update System${COLOR_OFF}\n"
+		softwareupdate -i -a
+	fi
 fi
