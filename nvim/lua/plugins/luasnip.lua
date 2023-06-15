27276cf7d84078029c2a6e6ec51f7fba8c7b4a64
Patrick Kahl <Kahl.Patrick@googlemail.com>
2023-06-16 00:05:25 +0200

    Add lia snippets

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -1,6 +1,8 @@
 return {
   "L3MON4D3/LuaSnip",
   config = function()
-    require("luasnip.loaders.from_vscode").lazy_load({ paths = "~/.config/nvim/my_snippets" })
+    require("luasnip.loaders.from_vscode").lazy_load({
+      paths = { "~/.config/nvim/my_snippets", "~/dev/lia-typo3-vscode" },
+    })
   end,
 }
