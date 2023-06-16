90a511609b2689d58d71f04f49cf840514797a45
Patrick Kahl <Kahl.Patrick@googlemail.com>
2023-06-16 11:49:37 +0200

    Update neovim LuaSnip to add all ~/dev/snippets subfolder

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -1,8 +1,18 @@
 return {
   "L3MON4D3/LuaSnip",
   config = function()
+    local snippet_paths = {"~/.config/nvim/my_snippets"}
+
+    -- use Neovim's globpath to get all subdirectories of ~/dev/snippets
+    local dirs = vim.fn.globpath("~/dev/snippets", "*/", 0, 1)
+
+    for _, dir in pairs(dirs) do
+      table.insert(snippet_paths, dir)
+    end
+
     require("luasnip.loaders.from_vscode").lazy_load({
-      paths = { "~/.config/nvim/my_snippets", "~/dev/lia-typo3-vscode" },
+      paths = snippet_paths,
     })
   end,
 }
+
