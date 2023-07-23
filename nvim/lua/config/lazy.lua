8de1f7dc91186385339966d28076759de8679ece
Patrick Kahl <Kahl.Patrick@googlemail.com>
2023-07-24 00:19:52 +0200

    Remove nvim flash because its know active by default

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -15,7 +15,6 @@ require("lazy").setup({
 
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
-    { import = "lazyvim.plugins.extras.editor.flash" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
