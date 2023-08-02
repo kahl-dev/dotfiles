3377840a491ce0bf1d098d0c9a2ea0b4f3c45a5e
Patrick Kahl <Kahl.Patrick@googlemail.com>
2023-08-02 15:42:02 +0200

    Update wezterm config

--- config/wezterm.lua
+++ config/wezterm.lua
@@ -1,32 +1,26 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This table will hold the configuration.
 local config = {}
-
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
+config.font = wezterm.font("FiraCode Nerd Font")
+config.font_size = 22
 
--- For example, changing the color scheme:
 config.color_scheme = "tokyonight"
-config.window_background_opacity = 0.9
-config.hide_tab_bar_if_only_one_tab = true
+
+config.enable_tab_bar = true
 config.tab_bar_at_bottom = true
-config.enable_scroll_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+
+config.window_background_opacity = 0.9
+
 config.send_composed_key_when_left_alt_is_pressed = true
 -- config.send_composed_key_when_right_alt_is_pressed = true
 -- config.use_ime = true
 
-config.font = wezterm.font("FiraCode Nerd Font")
-config.font_size = 22
-
 config.scrollback_lines = 50000
 config.window_decorations = "RESIZE"
 
--- and finally, return the configuration to wezterm
 return config
