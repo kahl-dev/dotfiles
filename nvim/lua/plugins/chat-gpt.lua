ee8718e1671f74ddfc48067f45c13a11418ab127
Patrick Kahl <Kahl.Patrick@googlemail.com>
2023-08-14 15:24:46 +0200

    Update chat-gpt plugin

--- nvim/lua/plugins/chat-gpt.lua
+++ nvim/lua/plugins/chat-gpt.lua
@@ -4,28 +4,35 @@
 
 return {
   "jackMort/ChatGPT.nvim",
+  event = "VeryLazy",
+  commit = "2107f70",
+
+  config = function()
+    require("chatgpt").setup()
+
+    -- require("chatgpt").setup({
+    --   api_key_cmd = "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",
+    -- })
+    -- chat = {
+    --   keymaps = {
+    --     close = { "jk", "kj", "<Esc>" },
+    --     yank_last = "<C-y>",
+    --     scroll_up = "<C-u>",
+    --     scroll_down = "<C-d>",
+    --     toggle_settings = "<C-o>",
+    --     new_session = "<C-n>",
+    --     cycle_windows = "<Tab>",
+    --   },
+    -- },
+    -- popup_input = {
+    --   -- submit = "<C-s>",
+    --   submit = "<CR>",
+    -- },
+  end,
+
   dependencies = {
     "MunifTanjim/nui.nvim",
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim",
   },
-  config = function()
-    require("chatgpt").setup({
-      chat = {
-        keymaps = {
-          close = { "jk", "kj", "<Esc>" },
-          yank_last = "<C-y>",
-          scroll_up = "<C-u>",
-          scroll_down = "<C-d>",
-          toggle_settings = "<C-o>",
-          new_session = "<C-n>",
-          cycle_windows = "<Tab>",
-        },
-      },
-      popup_input = {
-        -- submit = "<C-s>",
-        submit = "<CR>",
-      },
-    })
-  end,
 }
