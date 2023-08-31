466c034d8b46354ad1a5fdea2aebe9b7da42c116
Patrick Kahl <Kahl.Patrick@googlemail.com>
2023-08-31 09:44:55 +0200

    Update copilot bugfix function

--- scripts/bugfixCopilot.sh
+++ scripts/bugfixCopilot.sh
@@ -4,7 +4,7 @@ source ./scripts/config.sh
 source ./scripts/functions.sh
 
 if _is_osx; then
-	sed -i '' 's/\["\\n"\]/["\\n\\n\\n"]/' $(ls ~/.local/share/nvim/lazy/copilot.lua/copilot/dist/agent.js)
+	sed -i '' 's/\["\\n"\]/["\\n\\n\\n"]/' ~/.local/share/nvim/lazy/copilot.lua/copilot/dist/agent.js
 else
-	sed -i 's/\["\\n"\]/["\\n\\n\\n"]/' $(ls ~/.local/share/nvim/lazy/copilot.lua/copilot/dist/agent.js)
+	sed -i 's/\["\\n"\]/["\\n\\n\\n"]/' ~/.local/share/nvim/lazy/copilot.lua/copilot/dist/agent.js
 fi
