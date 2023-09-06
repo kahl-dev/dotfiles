27b82ca576b9934ccc97fe09496452c3ade5de34
Patrick Kahl <Kahl.Patrick@googlemail.com>
2023-09-06 17:53:55 +0200

    Update wezterm settings

--- config/wezterm.lua
+++ config/wezterm.lua
@@ -13,6 +13,7 @@ config.color_scheme = "tokyonight"
 config.enable_tab_bar = true
 config.tab_bar_at_bottom = true
 config.hide_tab_bar_if_only_one_tab = true
+config.adjust_window_size_when_changing_font_size = false
 
 config.window_background_opacity = 0.9
 
@@ -23,5 +24,12 @@ config.use_dead_keys = false
 
 config.scrollback_lines = 50000
 config.window_decorations = "RESIZE"
+
+config.keys = {
+	{ key = "a", mods = "ALT|CMD", action = { SendKey = { key = "ä" } } },
+	{ key = "u", mods = "ALT|CMD", action = { SendKey = { key = "ü" } } },
+	{ key = "o", mods = "ALT|CMD", action = { SendKey = { key = "ö" } } },
+	{ key = "s", mods = "ALT|CMD", action = { SendKey = { key = "ß" } } },
+}
 
 return config
