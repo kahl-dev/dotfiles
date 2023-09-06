8dc7faa2f9e79fbb87f425bb6148557bba4353a6
Patrick Kahl <Kahl.Patrick@googlemail.com>
2023-09-06 17:05:44 +0200

    Add ltex (languageTool) as spell checker

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,6 +1,10 @@
 -- Quickstart configs for Nvim LSP
 -- https://github.com/neovim/nvim-lspconfig
 
+-- Retrieve values from environment variables
+local username_from_env = os.getenv("LTEX_USERNAME")
+local apiKey_from_env = os.getenv("LTEX_APIKEY")
+
 return {
   "neovim/nvim-lspconfig",
   opts = {
@@ -12,6 +16,24 @@ return {
       -- https://github.com/aca/emmet-ls
       emmet_ls = {},
       marksman = {},
+      ltex = {
+        enabled = { "latex", "tex", "bib", "md" },
+        checkFrequency = "save",
+        diagnosticSeverity = "information",
+        setenceCacheSize = 5000,
+        -- language = "en-US",
+        -- language = "de-DE",
+        -- additionalRules = {
+        --   enablePickyRules = true,
+        --   motherTongue = "de-DE",
+        -- },
+        trace = { server = "verbose" },
+        languageToolOrg = {
+          username = username_from_env, -- Set the value from the env variable
+          apiKey = apiKey_from_env, -- Set the value from the env variable
+        },
+        completionEnabled = true,
+      },
     },
     format = {
       async = true,
