d22bf71decbc39248db4d5acc588d2766be0541e
Patrick Kahl <Kahl.Patrick@googlemail.com>
2023-09-22 15:15:26 +0200

    refactor: vim alias

--- zsh/config/vim.zsh
+++ zsh/config/vim.zsh
@@ -2,10 +2,6 @@ if _exec_exists nvim; then
   export EDITOR="nvim"
   export VISUAL="nvim"
 
-  # alias vim='nvm use default && nvim'
-  # alias vim='fnm exec --using=v18 nvim'
-
-
   function v() {
     if _exec_exists fnm; then
       command fnm exec --using=lts-latest nvim "$@"
@@ -18,3 +14,4 @@ if _exec_exists nvim; then
   alias vim='v'
   alias vimdiff='nvim -d'
 fi
+
