080fe05a7943d6d724a1629351b5f6be495545f6
Patrick Kahl <Kahl.Patrick@googlemail.com>
2023-09-29 07:53:31 +0200

    feat: try out gpt-4

--- nvim/lua/plugins/chat-gpt.lua
+++ nvim/lua/plugins/chat-gpt.lua
@@ -5,35 +5,39 @@
 return {
   "jackMort/ChatGPT.nvim",
   event = "VeryLazy",
-  commit = "2107f70",
-
-  config = function()
-    require("chatgpt").setup()
-
-    -- Set the key as OPENAI_API_KEY in your shell
-    -- require("chatgpt").setup({
-    --   api_key_cmd = "",
-    -- })
-    -- chat = {
-    --   keymaps = {
-    --     close = { "jk", "kj", "<Esc>" },
-    --     yank_last = "<C-y>",
-    --     scroll_up = "<C-u>",
-    --     scroll_down = "<C-d>",
-    --     toggle_settings = "<C-o>",
-    --     new_session = "<C-n>",
-    --     cycle_windows = "<Tab>",
-    --   },
-    -- },
-    -- popup_input = {
-    --   -- submit = "<C-s>",
-    --   submit = "<CR>",
-    -- },
-  end,
+  -- commit = "2107f70",
 
   dependencies = {
     "MunifTanjim/nui.nvim",
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim",
   },
+
+  config = function()
+    -- require("chatgpt").setup()
+
+    -- Set the key as OPENAI_API_KEY in your shell
+    require("chatgpt").setup({
+      --   api_key_cmd = "",
+      -- })
+      -- chat = {
+      --   keymaps = {
+      --     close = { "jk", "kj", "<Esc>" },
+      --     yank_last = "<C-y>",
+      --     scroll_up = "<C-u>",
+      --     scroll_down = "<C-d>",
+      --     toggle_settings = "<C-o>",
+      --     new_session = "<C-n>",
+      --     cycle_windows = "<Tab>",
+      --   },
+      -- },
+      -- popup_input = {
+      --   -- submit = "<C-s>",
+      --   submit = "<CR>",
+      -- },
+      openai_params = {
+        model = "gpt-4",
+      },
+    })
+  end,
 }
