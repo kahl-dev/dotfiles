4742a77510280408a07044574071231a1b75a63c
Patrick Kahl <Kahl.Patrick@googlemail.com>
2023-10-28 12:27:49 +0200

    refactor(nvim): Change the way wezterm config is written

--- config/wezterm.lua
+++ config/wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require("wezterm")
 
-function scheme_for_appearance(appearance)
+local function scheme_for_appearance(appearance)
 	if appearance:find("Dark") then
 		return "Catppuccin Mocha"
 	else
@@ -8,43 +8,34 @@ function scheme_for_appearance(appearance)
 	end
 end
 
--- return {
---   -- ...your existing config
--- }
+return {
+	-- Styles
+	font = wezterm.font("FiraCode Nerd Font"),
+	font_size = 16,
+	line_height = 1.1,
+	color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
+	-- color_scheme = "tokyonight"
 
-local config = {}
-if wezterm.config_builder then
-	config = wezterm.config_builder()
-end
+	-- window layout
+	enable_tab_bar = true,
+	tab_bar_at_bottom = true,
+	hide_tab_bar_if_only_one_tab = true,
+	adjust_window_size_when_changing_font_size = false,
+	window_background_opacity = 0.85,
+	macos_window_background_blur = 25,
 
-config.font = wezterm.font("FiraCode Nerd Font")
-config.font_size = 16
-config.line_height = 1.1
+	-- send_composed_key_when_left_alt_is_pressed = true
+	-- send_composed_key_when_right_alt_is_pressed = true
+	-- use_ime = true
+	use_dead_keys = false,
 
--- config.color_scheme = "tokyonight"
-config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+	scrollback_lines = 50000,
+	window_decorations = "RESIZE",
 
-config.enable_tab_bar = true
-config.tab_bar_at_bottom = true
-config.hide_tab_bar_if_only_one_tab = true
-config.adjust_window_size_when_changing_font_size = false
-
-config.window_background_opacity = 0.75
-config.macos_window_background_blur = 25
-
--- config.send_composed_key_when_left_alt_is_pressed = true
--- config.send_composed_key_when_right_alt_is_pressed = true
--- config.use_ime = true
-config.use_dead_keys = false
-
-config.scrollback_lines = 50000
-config.window_decorations = "RESIZE"
-
-config.keys = {
-	{ key = "a", mods = "ALT|CMD", action = { SendKey = { key = "ä" } } },
-	{ key = "u", mods = "ALT|CMD", action = { SendKey = { key = "ü" } } },
-	{ key = "o", mods = "ALT|CMD", action = { SendKey = { key = "ö" } } },
-	{ key = "s", mods = "ALT|CMD", action = { SendKey = { key = "ß" } } },
+	keys = {
+		{ key = "a", mods = "ALT|CMD", action = { SendKey = { key = "ä" } } },
+		{ key = "u", mods = "ALT|CMD", action = { SendKey = { key = "ü" } } },
+		{ key = "o", mods = "ALT|CMD", action = { SendKey = { key = "ö" } } },
+		{ key = "s", mods = "ALT|CMD", action = { SendKey = { key = "ß" } } },
+	},
 }
-
-return config
