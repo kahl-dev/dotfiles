2c76bc0a33dc56119afd8fa96f3168e93102ea71
Patrick Kahl <Kahl.Patrick@googlemail.com>
2023-10-28 12:16:47 +0200

    feat(nvim): Add transparent and wezterm options to catppucino

--- config/wezterm.lua
+++ config/wezterm.lua
@@ -1,5 +1,17 @@
 local wezterm = require("wezterm")
 
+function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Catppuccin Mocha"
+	else
+		return "Catppuccin Latte"
+	end
+end
+
+-- return {
+--   -- ...your existing config
+-- }
+
 local config = {}
 if wezterm.config_builder then
 	config = wezterm.config_builder()
@@ -10,6 +22,7 @@ config.font_size = 16
 config.line_height = 1.1
 
 -- config.color_scheme = "tokyonight"
+config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 
 config.enable_tab_bar = true
 config.tab_bar_at_bottom = true
--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -5,4 +5,14 @@ return {
       colorscheme = "catppuccin",
     },
   },
+
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = function(_, opts)
+      opts.transparent_background = true
+      return opts
+    end,
+  },
 }
