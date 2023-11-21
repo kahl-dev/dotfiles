63f81b7e7325e8af333f34892424e962ae93a303
Patrick Kahl <patrick@kahl.dev>
2023-11-21 08:44:34 +0100

    feat(nvim): Add autocmd to highlight copied text

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Add custom autocmds group
+vim.api.nvim_create_augroup("custom_buffer", { clear = true })
+-- Highlight yanked text
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = "custom_buffer",
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({ timeout = 200 })
+  end,
+})
