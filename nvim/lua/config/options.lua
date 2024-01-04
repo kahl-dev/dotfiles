0419241f71010768248df92d1789f9e18f74ddfc
Patrick Kahl <patrick@kahl.dev>
2024-01-04 09:04:44 +0100

    feat(nvim): Change project root detection

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,5 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.root_spec = { { ".git" }, "cwd" }
 -- vim.g.mapleader = ","
 -- vim.g.maplocalleader = ","
