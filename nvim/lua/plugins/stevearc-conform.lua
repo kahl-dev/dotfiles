088e0d64ecdb39496a7225a2abbf3e1fbd0169fb
Patrick Kahl <patrick@kahl.dev>
2024-03-04 10:39:44 +0100

    feat(nvim): update prettier nvim auto formation and exclude js and ts

---
+++ nvim/lua/plugins/stevearc-conform.lua
@@ -0,0 +1,26 @@
+-- LazyVim configuration for stevearc/conform.nvim
+-- https://www.lazyvim.org/extras/formatting/prettier
+
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      ["javascript"] = {},
+      ["javascriptreact"] = { "prettier" },
+      ["typescript"] = {},
+      ["typescriptreact"] = { "prettier" },
+      ["vue"] = { "prettier" },
+      ["css"] = { "prettier" },
+      ["scss"] = { "prettier" },
+      ["less"] = { "prettier" },
+      ["html"] = { "prettier" },
+      ["json"] = { "prettier" },
+      ["jsonc"] = { "prettier" },
+      ["yaml"] = { "prettier" },
+      ["markdown"] = { "prettier" },
+      ["markdown.mdx"] = { "prettier" },
+      ["graphql"] = { "prettier" },
+      ["handlebars"] = { "prettier" },
+    },
+  },
+}
