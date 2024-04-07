83c38453af99c44d2c871369b5e2dcd864e38f63
Patrick Kahl <patrick@kahl.dev>
2024-04-07 13:26:24 +0200

    feat(tmux): make it possible to share nvim content from one tmux pane to another

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -6,25 +6,17 @@ vim.g.root_spec = { { ".git" }, "cwd" }
 -- vim.g.mapleader = ","
 -- vim.g.maplocalleader = ","
 
--- Copy/Paste when using ssh on a remote server
--- Only works on Neovim >= 0.10.0
--- if vim.clipboard and vim.clipboard.osc52 then
---   vim.api.nvim_create_autocmd("VimEnter", {
---     group = augroup("ssh_clipboard"),
---     callback = function()
---       if vim.env.SSH_CONNECTION and vim.clipboard.osc52 then
---         vim.g.clipboard = {
---           name = "OSC 52",
---           copy = {
---             ["+"] = require("vim.clipboard.osc52").copy,
---             ["*"] = require("vim.clipboard.osc52").copy,
---           },
---           paste = {
---             ["+"] = require("vim.clipboard.osc52").paste,
---             ["*"] = require("vim.clipboard.osc52").paste,
---           },
---         }
---       end
---     end,
---   })
--- end
+if vim.env.SSH_CONNECTION then
+  vim.g.clipboard = {
+    name = "Tmux clipboard",
+    copy = {
+      ["+"] = { "tmux", "load-buffer", "-" },
+      ["*"] = { "tmux", "load-buffer", "-" },
+    },
+    paste = {
+      ["+"] = { "tmux", "save-buffer", "-" },
+      ["*"] = { "tmux", "save-buffer", "-" },
+    },
+    cache_enabled = 1,
+  }
+end
