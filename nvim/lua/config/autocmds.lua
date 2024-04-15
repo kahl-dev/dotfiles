bf14c84085922e053279d2bcc67015fc61570bd0
Patrick Kahl <patrick@kahl.dev>
2024-04-15 09:34:30 +0200

    feat(nvim): set zsh filetype to bash

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -12,3 +12,10 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank({ timeout = 200 })
   end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "zsh",
+  callback = function()
+    vim.bo.filetype = "bash"
+  end,
+})
