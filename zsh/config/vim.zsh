6e5e52d0cad69f93141c8621697a71b1500a7f27
Patrick Kahl <patrick@kahl.dev>
2024-04-28 16:16:54 +0200

    feat(zsh): add warning if nvim log file is to big until i fixed the problem

--- zsh/config/vim.zsh
+++ zsh/config/vim.zsh
@@ -19,3 +19,9 @@ if _exec_exists nvim; then
   alias vimdiff='nvim -d'
 fi
 
+# check if file /home/kahl/.local/state/nvim/log is greater that 1GB and if so echo a warning
+if [ -f /home/kahl/.local/state/nvim/log ]; then
+  if [ $(stat -c %s /home/kahl/.local/state/nvim/log) -gt 1073741824 ]; then # 1GB
+    echo "nvim log file is greater than 1GB"
+  fi
+fi
