0e13b38db17d93d1fcf6d7b44b4ed54d537c507d
Patrick Kahl <patrick@kahl.dev>
2024-04-28 16:14:10 +0200

    refactor: refactor nvim install from source script

--- scripts/installNvimFromSource.sh
+++ scripts/installNvimFromSource.sh
@@ -4,9 +4,9 @@ fi
 
 git clone git@github.com:neovim/neovim.git ~/$DOTFILES/build/neovim
 cd ~/$DOTFILES/build/neovim
-make CMAKE_BUILD_TYPE=Release 
+make CMAKE_BUILD_TYPE=Release
 #CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$DOTFILES/bin/nvim"
 rm -Rf $DOTFILES/bin/nvim
-make install CMAKE_INSTALL_PREFIX=$DOTFILES/bin/nvim
+make CMAKE_INSTALL_PREFIX=$DOTFILES/bin/nvim install
 cd -
 rm -rf ~/$DOTFILES/build/neovim
