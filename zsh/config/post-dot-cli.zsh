9ffa5e2d76864433e3c1f3352660482272bebbd0
Patrick Kahl <patrick@kahl.dev>
2024-04-28 20:45:21 +0200

    fix(dot-cli): fix cli integration

--- zsh/config/post-dot-cli.zsh
+++ zsh/config/post-dot-cli.zsh
@@ -1,9 +1,9 @@
-# alias dot-cli='node $DOTFILES/bin/dot-cli/src/index.mjs'
+! _is_path_exists "$DOTFILES/bin/dot-cli/node_modules/" && cd "$DOTFILES/bin/dot-cli" && pnpm install
 
-if _exec_exists "dot-cli"; then
-  alias dc='dot-cli'
-  zsh_add_completion "dot-cli" true
-fi
+alias dot-cli="node $DOTFILES/bin/dot-cli/src/index.mjs"
+alias dc="dot-cli"
+zsh_add_completion "dot-cli" true
+
 
 
 
