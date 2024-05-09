e029634747febd0991a8e70f4788653e8786eb5b
Patrick Kahl <patrick@kahl.dev>
2024-05-09 10:49:52 +0200

    fix(nvim): nvim from source bash script

--- scripts/installNvimFromSource.sh
+++ scripts/installNvimFromSource.sh
@@ -1,12 +1,17 @@
-if [ ! -d ~/$DOTFILES/build ]; then
-	mkdir ~/$DOTFILES/build
-fi
+[ ! -d "$DOTFILES/build" ] && mkdir -p "$DOTFILES/build"
 
-git clone git@github.com:neovim/neovim.git ~/$DOTFILES/build/neovim
-cd ~/$DOTFILES/build/neovim
-make CMAKE_BUILD_TYPE=Release
-#CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$DOTFILES/bin/nvim"
-rm -Rf $DOTFILES/bin/nvim
-make CMAKE_INSTALL_PREFIX=$DOTFILES/bin/nvim install
-cd -
-rm -rf ~/$DOTFILES/build/neovim
+rm -f ~/.local/state/nvim/log
+rm -f ~/.local/state/nvim/*.log
+
+git clone git@github.com:neovim/neovim.git "$DOTFILES/build/neovim"
+cd "$DOTFILES/build/neovim"
+# git checkout stable
+
+rm -rf "$DOTFILES/bin/nvim"
+make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX="$DOTFILES/bin/nvim"
+
+cd $DOTFILES/build/neovim
+make install
+
+cd $DOTFILES
+rm -rf "$DOTFILES/build/neovim"
