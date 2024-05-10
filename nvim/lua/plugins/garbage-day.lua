163ebdb6d38a7b73765b317082ed25be63844b24
Patrick Kahl <patrick@kahl.dev>
2024-05-10 21:01:58 +0200

    feat(nvim): update garbage collection plugin to ignore copilot and tweek some settings

--- nvim/lua/plugins/garbage-day.lua
+++ nvim/lua/plugins/garbage-day.lua
@@ -6,7 +6,19 @@ return {
     dependencies = "neovim/nvim-lspconfig",
     event = "VeryLazy",
     opts = {
+      -- Available options
+      -- https://github.com/Zeioth/garbage-day.nvim?tab=readme-ov-file#available-options
+      -- aggressive_mode = false,
+      excluded_lsp_clients = { "copilot" },
+      grace_period = 60 * 30, -- Seconds
+      -- wakeup_delay = 0,
+
+      -- Debug options
+      -- https://github.com/Zeioth/garbage-day.nvim?tab=readme-ov-file#debug-options
+      -- aggressive_mode_ignore = ,
       notifications = true,
+      retries = 5,
+      timeout = 1000, -- milliseconds
     },
   },
 }
