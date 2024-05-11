7f8318fafd54712c2ef4252b42c90726742fb865
Patrick Kahl <patrick@kahl.dev>
2024-05-11 10:17:16 +0200

    style(zsh): format code

--- zsh/config/zsh-autosuggestions.zsh
+++ zsh/config/zsh-autosuggestions.zsh
@@ -3,9 +3,7 @@
 
 zsh_add_plugin "zsh-users/zsh-autosuggestions"
 
+ZSH_AUTOSUGGEST_USE_ASYNC=true
 
-    ZSH_AUTOSUGGEST_USE_ASYNC=true
-
-
-    bindkey '^z' autosuggest-toggle # ctrl + z; Toggles between enabled/disabled suggestions.
-    bindkey '^f' autosuggest-accept # accept suggestions
+bindkey '^z' autosuggest-toggle # ctrl + z; Toggles between enabled/disabled suggestions.
+bindkey '^f' autosuggest-accept # accept suggestions
