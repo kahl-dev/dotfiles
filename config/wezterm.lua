8e7255c2b342e0648e611ebd6147a72fe5d5260f
Patrick Kahl <patrick@kahl.dev>
2024-05-17 19:47:03 +0200

    feat: update wezterm config

--- config/wezterm.lua
+++ config/wezterm.lua
@@ -1,41 +1,131 @@
 local wezterm = require("wezterm")
 
-local function scheme_for_appearance(appearance)
-	if appearance:find("Dark") then
-		return "Catppuccin Mocha"
-	else
-		return "Catppuccin Latte"
-	end
+-- https://www.youtube.com/watch?v=I3ipo8NxsjY
+-- https://github.com/theopn/dotfiles/blob/main/wezterm/wezterm.lua
+-- https://wezfurlong.org/wezterm/config/lua/general.html
+
+local config = {}
+if wezterm.config_builder then
+	config = wezterm.config_builder()
 end
 
-return {
-	-- Styles
-	font = wezterm.font("FiraCode Nerd Font"),
-	font_size = 16,
-	line_height = 1.1,
-	color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
-	-- color_scheme = "tokyonight"
+-- config.color_scheme = "Tokyo Night"
+config.color_scheme = "Catppuccin Mocha"
 
-	-- window layout
-	enable_tab_bar = true,
-	tab_bar_at_bottom = true,
-	hide_tab_bar_if_only_one_tab = true,
-	adjust_window_size_when_changing_font_size = false,
-	window_background_opacity = 0.85,
-	macos_window_background_blur = 25,
+config.font = wezterm.font_with_fallback({
+	{ family = "FiraCode Nerd Font", scale = 1.4 },
+	{ family = "JetBrains Mono", scale = 1.4 },
+	{ family = "Hack Nerd Font", scale = 1.4 },
+	{ family = "Menlo", scale = 1.4 },
+})
+-- config.font_size = 16
+-- config.line_height = 1.1
 
-	-- send_composed_key_when_left_alt_is_pressed = true
-	-- send_composed_key_when_right_alt_is_pressed = true
-	-- use_ime = true
-	use_dead_keys = false,
+config.term = "xterm-256color"
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 25
+config.window_decorations = "RESIZE"
+config.window_close_confirmation = "AlwaysPrompt"
+config.scrollback_lines = 3000
+config.default_workspace = "main"
 
-	scrollback_lines = 50000,
-	window_decorations = "RESIZE",
+config.use_dead_keys = false
 
-	keys = {
-		{ key = "a", mods = "ALT|CMD", action = { SendKey = { key = "ä" } } },
-		{ key = "u", mods = "ALT|CMD", action = { SendKey = { key = "ü" } } },
-		{ key = "o", mods = "ALT|CMD", action = { SendKey = { key = "ö" } } },
-		{ key = "s", mods = "ALT|CMD", action = { SendKey = { key = "ß" } } },
-	},
+-- Dim inactive panes
+config.inactive_pane_hsb = {
+	saturation = 0.24,
+	brightness = 0.5,
 }
+
+-- Tab bar
+-- I don't like the look of "fancy" tab bar
+config.use_fancy_tab_bar = false
+config.status_update_interval = 1000
+config.tab_bar_at_bottom = true
+config.hide_tab_bar_if_only_one_tab = false
+wezterm.on("update-status", function(window, pane)
+	-- Workspace name
+	local stat = window:active_workspace()
+	local stat_color = "#f7768e"
+	-- It's a little silly to have workspace name all the time
+	-- Utilize this to display LDR or current key table name
+	if window:active_key_table() then
+		stat = window:active_key_table()
+		stat_color = "#7dcfff"
+	end
+	if window:leader_is_active() then
+		stat = "LDR"
+		stat_color = "#bb9af7"
+	end
+
+	local basename = function(s)
+		-- Nothing a little regex can't fix
+		return string.gsub(s, "(.*[/\\])(.*)", "%2")
+	end
+
+	-- Current working directory
+	local cwd = pane:get_current_working_dir()
+	if cwd then
+		if type(cwd) == "userdata" then
+			-- Wezterm introduced the URL object in 20240127-113634-bbcac864
+			cwd = basename(cwd.file_path)
+		else
+			-- 20230712-072601-f4abf8fd or earlier version
+			cwd = basename(cwd)
+		end
+	else
+		cwd = ""
+	end
+
+	-- Current command
+	local cmd = pane:get_foreground_process_name()
+	-- CWD and CMD could be nil (e.g. viewing log using Ctrl-Alt-l)
+	cmd = cmd and basename(cmd) or ""
+
+	-- Time
+	local time = wezterm.strftime("%H:%M")
+
+	-- Left status (left of the tab line)
+	window:set_left_status(wezterm.format({
+		{ Foreground = { Color = stat_color } },
+		{ Text = "  " },
+		{ Text = wezterm.nerdfonts.oct_table .. "  " .. stat },
+		{ Text = " |" },
+	}))
+
+	-- Right status
+	window:set_right_status(wezterm.format({
+		-- Wezterm has a built-in nerd fonts
+		-- https://wezfurlong.org/wezterm/config/lua/wezterm/nerdfonts.html
+		{ Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
+		{ Text = " | " },
+		{ Foreground = { Color = "#e0af68" } },
+		{ Text = wezterm.nerdfonts.fa_code .. "  " .. cmd },
+		"ResetAttributes",
+		{ Text = " | " },
+		{ Text = wezterm.nerdfonts.md_clock .. "  " .. time },
+		{ Text = "  " },
+	}))
+end)
+
+config.enable_tab_bar = true
+config.window_padding = {
+	left = "0.5cell",
+	right = "0.5cell",
+	top = "1.5cell",
+	bottom = "0cell",
+}
+
+return config
+
+-- -- send_composed_key_when_left_alt_is_pressed = true
+-- -- send_composed_key_when_right_alt_is_pressed = true
+-- -- use_ime = true
+-- use_dead_keys = false,
+--
+-- keys = {
+-- 	{ key = "a", mods = "ALT|CMD", action = { SendKey = { key = "ä" } } },
+-- 	{ key = "u", mods = "ALT|CMD", action = { SendKey = { key = "ü" } } },
+-- 	{ key = "o", mods = "ALT|CMD", action = { SendKey = { key = "ö" } } },
+-- 	{ key = "s", mods = "ALT|CMD", action = { SendKey = { key = "ß" } } },
+-- },
