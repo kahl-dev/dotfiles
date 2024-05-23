a97ba8b10489e0351c899dc84db40e0500652a2e
Patrick Kahl <patrick@kahl.dev>
2024-05-23 17:24:09 +0200

    feat: update brew setup
    - ignore installation if its already installed
    - add yabai

--- scripts/setup_homebrew.zsh
+++ scripts/setup_homebrew.zsh
@@ -7,15 +7,15 @@ echo "\n<<< Starting Homebrew setup >>>\n"
 if is_macos; then
 
   if command_exists brew; then
-    prompt_user "Homebrew is already installed. Do you want to reinstall it?" \
-      '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"' \
-      'eval "$(/opt/homebrew/bin/brew shellenv)"'
+    echo 'Homebrew is already installed. Skipping installation.'
   else
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     eval "$(/opt/homebrew/bin/brew shellenv)"
   fi
 
+  yabai --uninstall-service
   brew bundle --verbose --file=$HOMEBREW_BUNDLE_FILE_GLOBAL
+  yabai --start-service
 fi
 
 # TODO: Test this on a Linux machine
