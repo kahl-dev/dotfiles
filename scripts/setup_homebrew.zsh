2215c6b011e200ec538c1e8eb5d5554f94ae0d98
Patrick Kahl <patrick@kahl.dev>
2024-05-23 08:40:31 +0200

    feat(core): move all link files to a tree that matches the target

--- scripts/setup_homebrew.zsh
+++ scripts/setup_homebrew.zsh
@@ -15,6 +15,20 @@ if is_macos; then
     eval "$(/opt/homebrew/bin/brew shellenv)"
   fi
 
-  echo $HOMEBREW_BUNDLE_FILE_GLOBAL
   brew bundle --verbose --file=$HOMEBREW_BUNDLE_FILE_GLOBAL
 fi
+
+# TODO: Test this on a Linux machine
+# if is_linux; then
+# 	LINUXBREWHOME=$HOME/.linuxbrew
+#
+#   if command_exists brew; then
+#     prompt_user "Homebrew is already installed. Do you want to reinstall it?" \
+#       '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"' \
+#       'eval "$(/opt/homebrew/bin/brew shellenv)"'
+#   else
+#     # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#     eval "$(/opt/homebrew/bin/brew shellenv)"
+#   fi
+#
+# fi
