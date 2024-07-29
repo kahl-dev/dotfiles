6597d1c54ca59079ff07e2926d8cc98046b85f9f
Patrick Kahl <patrick@kahl.dev>
2024-07-29 20:12:10 +0200

    feat(zsh): add atuin

--- zsh/config/plugins.zsh
+++ zsh/config/plugins.zsh
@@ -24,6 +24,10 @@ zinit light hlissner/zsh-autopair
 # https://github.com/ajeetdsouza/zoxide
 command_exists zoxide && eval "$(zoxide init --cmd cd zsh)"
 
+zinit ice as"command" from"gh-r" bpick"atuin-*.tar.gz" mv"atuin*/atuin -> atuin" \
+    atclone"./atuin init zsh > init.zsh; ./atuin gen-completions --shell zsh > _atuin" \
+    atpull"%atclone" src"init.zsh"
+zinit light atuinsh/atuin
 
 # zinit light ajeetdsouza/zoxide
 
