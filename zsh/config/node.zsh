b1a8dd0fde82873306011ba38b83444d5f9ce895
Patrick Kahl <patrick@kahl.dev>
2024-09-07 16:06:34 +0200

    feat(zsh): remove pnpm settings

--- zsh/config/node.zsh
+++ zsh/config/node.zsh
@@ -1,9 +1,7 @@
 # npm aliases and options
 
-
 # Fast and simple Node.js version manager, built in Rust
 # https://github.com/Schniz/fnm
-
 
 if command_exists fnm; then
   eval "$(fnm env --use-on-cd --version-file-strategy=recursive)"
@@ -22,8 +20,8 @@ if command_exists fnm; then
     echo "Uninstalling all installed Node versions..."
     # Parsing 'fnm list' output to accurately extract version numbers
     for version in $(fnm list | grep -o 'v[0-9]*\.[0-9]*\.[0-9]*'); do
-        echo "Uninstalling Node version $version"
-        fnm uninstall "$version"
+      echo "Uninstalling Node version $version"
+      fnm uninstall "$version"
     done
   }
 
@@ -34,26 +32,26 @@ fi
 
 # PNPM
 
-export PNPM_HOME="$HOME/.local/share/pnpm"
-
-case ":$PATH:" in
-  *":$PNPM_HOME:"*) ;;
-  *) export PATH="$PNPM_HOME:$PATH" ;;
-esac
-
-# tabtab source for pnpm package
-# uninstall by removing these lines
-[[ -f ~/.config/tabtab/zsh/__tabtab.zsh ]] && . ~/.config/tabtab/zsh/__tabtab.zsh || true
-
-if [ "$(pnpm config get store-dir)" != "$PNPM_HOME" ]; then
-  echo "pnpm store not set to \$PNPM_HOME, setting now..."
-  echo "pnpm config set store-dir \"$PNPM_HOME\""
-
-  # Create path if it doesn't exist
-  if [ ! -d "$PNPM_HOME" ]; then
-    echo "Creating pnpm store directory at $PNPM_HOME"
-    mkdir -p "$PNPM_HOME"
-  fi
-
-  pnpm config set store-dir "$PNPM_HOME"
-fi
+# export PNPM_HOME="$HOME/.local/share/pnpm"
+#
+# case ":$PATH:" in
+#   *":$PNPM_HOME:"*) ;;
+#   *) export PATH="$PNPM_HOME:$PATH" ;;
+# esac
+#
+# # tabtab source for pnpm package
+# # uninstall by removing these lines
+# [[ -f ~/.config/tabtab/zsh/__tabtab.zsh ]] && . ~/.config/tabtab/zsh/__tabtab.zsh || true
+#
+# if [ "$(pnpm config get store-dir)" != "$PNPM_HOME" ]; then
+#   echo "pnpm store not set to \$PNPM_HOME, setting now..."
+#   echo "pnpm config set store-dir \"$PNPM_HOME\""
+#
+#   # Create path if it doesn't exist
+#   if [ ! -d "$PNPM_HOME" ]; then
+#     echo "Creating pnpm store directory at $PNPM_HOME"
+#     mkdir -p "$PNPM_HOME"
+#   fi
+#
+#   pnpm config set store-dir "$PNPM_HOME"
+# fi
