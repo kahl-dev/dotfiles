e4ad0e0c7c8ca258e3a931e4e603c44716e7a391
Patrick Kahl <patrick@kahl.dev>
2025-01-15 12:16:47 +0100

    feat(nvim): update blink-cmp config

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -1,51 +1,30 @@
 return {
   {
     "saghen/blink.cmp",
-    version = "0.8.2", -- Plugin version for compatibility.
-    -- opts = function(_, opts)
-    --   -- Ensures the specified nested path exists in a table, creating tables as needed.
-    --   local function ensure_path(obj, path)
-    --     for _, key in ipairs(path) do
-    --       obj[key] = obj[key] or {} -- Create the table if it doesn't exist.
-    --       obj = obj[key] -- Navigate deeper into the structure.
-    --     end
-    --     return obj -- Return the final table for further modifications.
-    --   end
-    --
-    --   local snippet_path = "/home/kahl/dev/snippets/lia-typo3-vscode"
-    --   if vim.fn.isdirectory(snippet_path) == 1 then
-    --     -- Ensure the nested structure exists and set the snippet search path.
-    --     ensure_path(opts, { "sources", "providers", "snippets", "opts" }).search_paths = { snippet_path }
-    --   end
-    --
-    --   -- Configure key mappings for navigating suggestions.
-    --   opts.keymap = {
-    --     ["<C-k>"] = { "select_prev", "fallback" },
-    --     ["<C-j>"] = { "select_next", "fallback" },
-    --   }
-    --
-    --   return opts -- Return the updated configuration.
-    -- end,
     opts = {
       keymap = {
         ["<C-j>"] = { "select_next", "fallback" },
         ["<C-k>"] = { "select_prev", "fallback" },
-        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+        -- ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
       },
-      -- source = {
-      --   providers = {
-      --     snippets = {
-      --       opts = {
-      --         search_paths = { "/home/kahl/dev/snippets/lia-typo3-vscode" },
-      --       },
-      --     },
-      --   },
-      -- },
+      sources = {
+        min_keyword_length = function(ctx)
+          return ctx.trigger.kind == "manual" and 0 or 2
+        end,
+        providers = {
+          snippets = {
+            opts = {
+              search_paths = { "/home/kahl/dev/snippets/lia-typo3-vscode" },
+            },
+          },
+        },
+      },
     },
-    -- opts_extend = { "sources.providers.snippets.opts.search_paths", "keymaps" },
-    opts_extend = { "keymaps" },
+
+    opts_extend = { "sources.providers.snippets.opts.search_paths", "keymaps" },
   },
 
+  -- workaround for make <C-k> work in saghen/blink.cmp
   {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
