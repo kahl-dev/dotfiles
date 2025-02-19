a48fffd26e4a3db42b207d36dde1ad9b9ac5a062
Patrick Kahl <patrick@kahl.dev>
2025-02-19 18:35:49 +0100

    feat: remove workaround that is not more needed

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -23,27 +23,4 @@ return {
 
     opts_extend = { "sources.providers.snippets.opts.search_paths", "keymaps" },
   },
-
-  -- workaround for make <C-k> work in saghen/blink.cmp
-  {
-    "neovim/nvim-lspconfig",
-    opts = function(_, opts)
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      keys[#keys + 1] = { "<C-k>", false, mode = { "i" } }
-
-      opts.servers.eslint.settings.workingDirectory = { mode = "auto" }
-      opts.inlay_hints = { enabled = false }
-      opts.setup.eslint = function()
-        require("lazyvim.util").lsp.on_attach(function(client)
-          if client.name == "eslint" then
-            client.server_capabilities.documentFormattingProvider = true
-          elseif client.name == "tsserver" then
-            client.server_capabilities.documentFormattingProvider = false
-          end
-        end)
-        vim.cmd([[autocmd BufWritePre *.tsx,*.ts,*.jsx,*.js EslintFixAll]])
-      end
-      return opts
-    end,
-  },
 }
