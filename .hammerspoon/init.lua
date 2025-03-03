d14e9c9139b1da91dbd3fc94add3c360b25efa05
Patrick Kahl <patrick@kahl.dev>
2025-03-03 23:44:54 +0100

    feat: add hammerspoon config to enable keyboard layer for opening apps

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,10 +1,32 @@
 -- Define a hotkey to reload Hammerspoon configuration
--- hs.hotkey.bind({ "cmd", "alt" }, "R", function()
--- 	hs.reload()
--- end)
--- hs.alert.show("Config loaded")
---
--- hs.loadSpoon("AClock")
--- hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "C", function()
--- 	spoon.AClock:toggleShow()
--- end)
+hs.hotkey.bind({ "cmd", "alt" }, "R", function()
+	hs.reload()
+end)
+hs.alert.show("Config loaded")
+
+local modal = hs.hotkey.modal.new({}, "F17") -- An unused key to represent the layer
+
+hs.hotkey.bind({ "shift" }, "space", function()
+	modal:enter()
+	hs.alert.show("Layer activated")
+end)
+
+modal:bind({}, "t", function()
+	hs.application.launchOrFocusByBundleID("com.mitchellh.ghostty")
+	modal:exit()
+end)
+
+modal:bind({}, "b", function()
+	hs.application.launchOrFocusByBundleID("company.thebrowser.Browser")
+	modal:exit()
+end)
+
+modal:bind({}, "m", function()
+	hs.application.launchOrFocusByBundleID("com.microsoft.teams2")
+	modal:exit()
+end)
+
+modal:bind({}, "g", function()
+	hs.application.launchOrFocusByBundleID("com.openai.chat")
+	modal:exit()
+end)
