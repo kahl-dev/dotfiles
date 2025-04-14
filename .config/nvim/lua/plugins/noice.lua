bfafd2848b24e74ee14e81d7fb5be2cc37be4711
Patrick Kahl <patrick@kahl.dev>
2025-04-14 10:06:23 +0200

    fix(nvim): hide annoying noice messages

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -3,12 +3,22 @@
 return {
   {
     "folke/noice.nvim",
+    event = "VeryLazy",
     opts = {
       lsp = {
         hover = {
           -- Set not show a message if hover is not available
           -- ex: shift+k on Typescript code
           silent = true,
+        },
+      },
+      routes = {
+        {
+          filter = {
+            event = "notify",
+            find = "Request textDocument/inlayHint failed",
+          },
+          opts = { skip = true },
         },
       },
     },
