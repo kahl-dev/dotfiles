ecab9bc3508e337c8d9025eb5eea2d7b3ef4b5de
Patrick Kahl <patrick@kahl.dev>
2025-06-18 17:40:34 +0200

    feat(hammerspoon): update hammerspoon config

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -4,29 +4,29 @@ hs.hotkey.bind({ "cmd", "alt" }, "R", function()
 end)
 hs.alert.show("Config loaded")
 
-local modal = hs.hotkey.modal.new({}, "F17") -- An unused key to represent the layer
-
-hs.hotkey.bind({ "shift" }, "space", function()
-	modal:enter()
-	hs.alert.show("Layer activated")
-end)
-
-modal:bind({}, "t", function()
-	hs.application.launchOrFocusByBundleID("com.mitchellh.ghostty")
-	modal:exit()
-end)
-
-modal:bind({}, "b", function()
-	hs.application.launchOrFocusByBundleID("company.thebrowser.Browser")
-	modal:exit()
-end)
-
-modal:bind({}, "m", function()
-	hs.application.launchOrFocusByBundleID("com.microsoft.teams2")
-	modal:exit()
-end)
-
-modal:bind({}, "g", function()
-	hs.application.launchOrFocusByBundleID("com.openai.chat")
-	modal:exit()
-end)
+-- local modal = hs.hotkey.modal.new({}, "F17") -- An unused key to represent the layer
+--
+-- hs.hotkey.bind({ "ctrl", "alt" }, "space", function()
+-- 	modal:enter()
+-- 	hs.alert.show("Layer activated")
+-- end)
+--
+-- modal:bind({}, "t", function()
+-- 	hs.application.launchOrFocusByBundleID("com.mitchellh.ghostty")
+-- 	modal:exit()
+-- end)
+--
+-- modal:bind({}, "b", function()
+-- 	hs.application.launchOrFocusByBundleID("company.thebrowser.Browser")
+-- 	modal:exit()
+-- end)
+--
+-- modal:bind({}, "m", function()
+-- 	hs.application.launchOrFocusByBundleID("com.microsoft.teams2")
+-- 	modal:exit()
+-- end)
+--
+-- modal:bind({}, "g", function()
+-- 	hs.application.launchOrFocusByBundleID("com.openai.chat")
+-- 	modal:exit()
+-- end)
