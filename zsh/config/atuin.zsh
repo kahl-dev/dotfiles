ac6139b4f1f97e43a45c7e2b02ff1ba06162fceb
Patrick Kahl <patrick@kahl.dev>
2025-07-05 18:41:49 +0200

    chore(atuin): update atuin configuration
    Minor updates to atuin configuration file

--- zsh/config/atuin.zsh
+++ zsh/config/atuin.zsh
@@ -2,6 +2,11 @@ if ! folder_exists ~/.atuin/bin && [[ $- == *i* ]]; then;
   curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
 fi
 
+if folder_exists ~/.atuin/bin; then;
+  . "$HOME/.atuin/bin/env"
+fi
+
 if command_exists atuin; then;
   eval "$(atuin init zsh)"
 fi
+
